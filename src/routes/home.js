2f8e521fdf354eae839b63408aa7877793fe4a6b
David Wilson <david@dvdwlsn.com>
2016-07-20 22:27:16 -0400

    Split my_friends into its own view and fix its route
    Fix relative paths so images and resources are loaded correctly.
    
    Resolves: Task #22

--- src/routes/home.js
+++ src/routes/home.js
@@ -92,68 +92,14 @@ const posts = [
 module.exports = [
   {
     method: 'GET',
-    path: '/home/my_friends',
+    path: '/home/my-friends',
     handler: function (request, reply) {
       const inBodyAds = [
         "one",
         "two"
       ];
-
-      reply.view('desktop_dash', {
-        messageSets: [
-          {
-            messageHeader: "My Replies",
-            messages: [
-              {
-                title: "Kid Clothes",
-                image: "http://lorempixel.com/250/250",
-                category: "offer",
-                type: "Reply",
-                time: '10 Minutes Ago',
-                id: 1
-              },
-              {
-                title: "Acrylic Paints",
-                image: "http://lorempixel.com/250/250",
-                category: "offer",
-                type: "Reply",
-                time: '15 Minutes Ago',
-                id: 2
-              }
-            ]
-          },
-          {
-            messageHeader: "Replies to My Posts",
-            messages: [
-              {
-                title: "Vaccum Cleaner with a very long title",
-                category: "wanted",
-                image: "http://lorempixel.com/250/250",
-                type: "PostReply",
-                time: '15 Minutes Ago',
-                notification: "7 unread replies",
-                id: 3
-              },
-              {
-                title: "Canon 560 Printer",
-                category: "lend",
-                image: "http://lorempixel.com/250/250",
-                type: "PostReply",
-                time: '15 Minutes Ago',
-                notification: "15 unread replies",
-                id: 5
-              },
-              {
-                title: "Patio Furniture",
-                category: "offer",
-                image: "http://lorempixel.com/250/250",
-                type: "PostReply",
-                time: '15 Minutes Ago',
-                notification: "1 unread reply",
-                id: 6
-              }
-            ]
-          },
+      reply.view('./home/my_friends', {
+        messageSets: [          
           {
             messageHeader: "Chat With Friends",
             messages: [
@@ -178,7 +124,7 @@ module.exports = [
         filterType: 'circle',
         friends: friends,
         inBodyAds: inBodyAds,
-        title: "Desktop Dash",
+        title: "My Friends",
         footerMenuItems: footerMenuItems,
         posts: posts
       });
