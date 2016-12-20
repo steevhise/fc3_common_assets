16e35cb6bf8df99b378d33811390c46333a1397d
Ryan Watts <ryandwatts@gmail.com>
2016-12-20 06:06:33 -0700

    [Task47-Homebar] - added js module for home tabs - added new partial home_tabs - moved home tabs to partial and replaced all existing references with partial - created switch statement to handle active tab state (there may), be a better way to do this. - tested use cases to ensure tabs are set properly and classes/attribute s are maintained.

---
+++ src/assets/js/modules/home_tabs.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+	// define tabRoot to ensure strict scoping
+	const tabRoot = $('#h-example-tabs');
+	
+	let setActiveTab = function (elm) {
+		$(elm).attr('aria-selected', 'true');
+		$(elm).closest('li').addClass('is-active');
+	};
+	
+	switch (document.location.pathname) {
+		case '/desktop-dash':
+			setActiveTab('#desktop-dash');
+			break;
+		case '/home/my-replies':
+			setActiveTab('#my-replies');
+			break;
+		case '/home/my-posts':
+			setActiveTab('#my-posts');
+			break;
+		case '/home/my-friends':
+			setActiveTab('#my-friends');
+			break;
+		case '/home/my-groups':
+			setActiveTab('#my-groups');
+			break;
+			
+	}
+	
+});
+
+export default "HomeTabs";
