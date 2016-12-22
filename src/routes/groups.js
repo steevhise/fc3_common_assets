a0b23bba98f4a76c25cbcaaafdf389c6c5b42132
Steev Hise <steev@detritus.net>
2016-12-22 15:01:28 -0700

    * added group page template and route stubs

---
+++ src/routes/groups.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// route definitions
+module.exports = [
+    {
+        method: 'GET',
+        path: '/group/{unique_group_name}',
+        config: {
+            id: 'group',
+            description: 'a group page, for example try /group/freecycle',
+            /*plugins: { 'auth-cookie-freecycle': {
+             redirectTo: false,
+             redirectOnTry: false
+             }}*/
+        },
+        handler: function (request, reply) {
+            const inBodyAds = [
+                "one",
+                "two"
+            ];
+
+            var unique_name = request.params.unique_group_name;
+            request.log('debug', 'about to look up group ' + unique_name);
+
+           /* new request.server.Group(unique_name, function(err, group) {
+                console.log('returned from  constructor:');
+                console.log(group);*/
+
+           // fake data for now
+            var group = { group_name: "Tucson",
+                unique_group_name: "freecycle",
+                num_members: 6900,
+                group_id: 1014,
+                posts: [ { post_id: 100}, {post_id:101}, {post_id:103}, {post_id:105} ]
+
+
+            };
+
+                reply.view('groups/group.html', {
+                    showFilterSelectors: true,
+                    inBodyAds: inBodyAds,
+                    // title: "Post #" + post_id,
+                    // footerMenuItems: footerMenuItems,
+                    group: group
+                });
+
+            // });
+        }
+    }
+];
