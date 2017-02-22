278c81189737e7d85e9b03531e303128565d995c
Steev Hise <steev@detritus.net>
2017-02-22 14:42:46 -0700

    * some hapi style related changes

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -1,4 +1,3 @@
-'use strict';
 
 // route definitions
 module.exports = [
@@ -7,19 +6,20 @@ module.exports = [
         path: '/group/{unique_group_name}',
         config: {
             id: 'group',
-            description: 'a group page, for example try /group/freecycle',
+            description: 'a group page, for example try /group/freecycle'
             /*plugins: { 'auth-cookie-freecycle': {
              redirectTo: false,
              redirectOnTry: false
              }}*/
         },
         handler: function (request, reply) {
+
             const inBodyAds = [
-                "one",
-                "two"
+                'one',
+                'two'
             ];
 
-            var unique_name = request.params.unique_group_name;
+            const unique_name = request.params.unique_group_name;
             request.log('debug', 'about to look up group ' + unique_name);
 
            /* new request.server.Group(unique_name, function(err, group) {
@@ -27,22 +27,20 @@ module.exports = [
                 console.log(group);*/
 
            // fake data for now
-            var group = { group_name: "Tucson",
-                unique_group_name: "freecycle",
+            const group = { group_name: 'Tucson',
+                unique_group_name: 'freecycle',
                 num_members: 6900,
                 group_id: 1014,
-                posts: [ { post_id: 100}, {post_id:101}, {post_id:103}, {post_id:105} ]
-
-
+                posts: [{ post_id: 100 }, { post_id:101 }, { post_id:103 }, { post_id:105 }]
             };
 
-                reply.view('groups/group.html', {
-                    showFilterSelectors: true,
-                    inBodyAds: inBodyAds,
+            reply.view('groups/group.html', {
+                showFilterSelectors: true,
+                inBodyAds,
                     // title: "Post #" + post_id,
                     // footerMenuItems: footerMenuItems,
-                    group: group
-                });
+                group
+            });
 
             // });
         }
