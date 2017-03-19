078ef974dd8221a1eb6617ca0b2f9627fb1243d2
Ryan Watts <ryandwatts@gmail.com>
2017-03-19 12:31:09 -0700

    [markers icons] \n We now have marker icons. \n - updates to home.js my-groups route to contain examples for different ways to configure a marker. \n - added a getIcon method to geomap \n - added an initIcons method to geomap \n - tested use case for passing a predefined marker user|post|group \n - tested use case for passing a custom marker configuration , it must be defined within an object with a key of custom for the icon property in home.js \n - tested use case for passing raw html directly to a marker for the popup description

--- src/assets/js/modules/geomap.js
+++ src/assets/js/modules/geomap.js
@@ -4,6 +4,7 @@ import gmap from 'google-maps';
 // default values that must be available to all map instances
 gmap.KEY = 'AIzaSyAF77Fi1J3JmCKUHxEGaGRDVit5E_CTAmQ';
 gmap.LIBRARIES = ['places'];
+
 /**
  * A class wrapper for geomap, that simplifies the instantiation process.
  * @class {null} GeoMap
@@ -13,17 +14,15 @@ export default class GeoMap {
 		
 		this.instance = instance;
 		this.$element = $(this.instance);
+		this.markerIcons = {};
 
 		this.defaultSettings = {
 			height: '400',
 			width: '100%'
 		};
 
-		this.markerIcons = {
-			groups : '',
-			posts : '',
-			users : '',
-		};
+		// initialize the default marker icons.
+		this.initIcons();
 
 		// this function builds the map.
 		this.init();
@@ -36,12 +35,7 @@ export default class GeoMap {
 	init() {
 		this.settings = JSON.parse(new Array(this.$element.attr('data-geomap-settings'))) || this.defaultSettings;
 		this.markers = JSON.parse(new Array(this.$element.attr('data-geomap-markers'))) || [];
-
-		// marker icons construction.
-		// this.markerIcons = {
-         //    group:
-        // };
-
+		
 		let geoLocation;
 		
 		navigator.geolocation.getCurrentPosition( (position) =>  {
@@ -68,10 +62,10 @@ export default class GeoMap {
 			let mapLocations = this.markers;
 			
 			// loop through the markers and attach them to the map instance
-			for (i = 0; i < mapLocations.length; i++) { 
+			for (i = 0; i < mapLocations.length; i++) {
 				marker = new google.maps.Marker({
 					position: new google.maps.LatLng(Number(mapLocations[i].lat), Number(mapLocations[i].lng)),
-					icon : 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png',
+					icon : this.getIcon(mapLocations[i]),
 					map: map
 				});
 
@@ -90,12 +84,73 @@ export default class GeoMap {
 		});
 	};
 
-	setMarkerIcon(icon) {
-		return Object.assign(this.markerIcons, icon);
+	/**
+	 * Initialize the setup of the default marker icons.
+	 * note a marker icon may be passed as an object, by which it will be used to register a new marker icon,
+	 * upon instantiation. 
+	 * A marker icon may also be passed a string, the string must be the url of a marker icon accessible via
+	 * the web.
+	 * 
+	 * @method initIcons
+	 * @return {null}
+	 */
+	initIcons() {
+		let settings = {
+			basePath : 'M10.75,0A6.25,6.25,0,0,0,4.5,6.25c0,6,6.25,13.75,6.25,13.75S17,12.22,17,6.25A6.25,6.25,0,0,0,10.75,0Zm0,9.7a3.38,3.38,0,1,1,3.38-3.38A3.37,3.37,0,0,1,10.75,9.7Z'
+		};
+
+		let icons = {
+			default : {
+				path : settings.basePath,
+				fillColor : '#34b233',
+				fillOpacity : 0.8,
+				strokeColor : 'white',
+				scale : 2
+			},
+			group : {
+				path : settings.basePath,
+				fillColor : '#ff5722',
+				fillOpacity : 0.8,
+				strokeColor : 'white',
+				scale : 2
+			},
+			user : {
+				path : settings.basePath,
+				fillColor : '#00bcd4',
+				fillOpacity : 0.8,
+				strokeColor : 'white',
+				scale : 2
+			},
+			post : {
+				path : settings.basePath,
+				fillColor : '#673AB7',
+				fillOpacity : 0.8,
+				strokeColor : 'white',
+				scale : 2
+			}
+		};
+
+		this.markerIcons = icons;
 	};
-	getMarkerIcon(icon) {
-		return this.markerIcons[icon];
-	};
+
+	/**
+	 * This function accepts a string or object with key custom.
+	 * Primarily used when building the map markers to determine,
+	 * which icon should be rendered with the marker.
+	 * 
+	 * @method getIcon
+	 * @param {string|object} config 
+	 */
+	getIcon(config) {
+		if (typeof config.icon === 'string') {
+			return this.markerIcons[config.icon];
+		} else if (typeof config.icon === 'object' && config.icon.custom !== undefined) {
+			return config.icon.custom;
+		} else {
+			// if all else fails return a default marker icon.
+			return this.markerIcons.default;
+		}
+	}
 	
 }
 
