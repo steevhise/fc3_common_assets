77a9d6a314c8ce28374ef8c906c5ff99e7e98e53
Ryan Watts <ryandwatts@gmail.com>
2017-03-19 15:37:49 -0700

    [marker icons] \n - added support for readmore text and path \n - added new method getReadMore \n - updated marker info window to pass read more button if one is present.

--- src/routes/home.js
+++ src/routes/home.js
@@ -38,6 +38,9 @@ const friends = [
     }
 ];
 
+// see google docs for custom svg settings, these are just a few mainly the
+// ones we would use to configure a new icon. but you can also pass things like maxWidth,
+// and other settings as well https://developers.google.com/maps/documentation/javascript/symbols
 const customIcon = {
     path : 'M10.75,0A6.25,6.25,0,0,0,4.5,6.25c0,6,6.25,13.75,6.25,13.75S17,12.22,17,6.25A6.25,6.25,0,0,0,10.75,0Zm0,9.7a3.38,3.38,0,1,1,3.38-3.38A3.37,3.37,0,0,1,10.75,9.7Z',
     fillColor : 'teal',
@@ -56,7 +59,8 @@ const myGroupsGeomap = {
         { 'lat' : 32.0, 'lng' : -111.09, 'description' : 'Oro Valley', 'icon' : 'user' },
         { 'lat' : 31.5, 'lng' : -110.09, 'description' : 'Green Valley', 'icon' : 'post' },
         { 'lat' : 33.5, 'lng' : -110.09, 'description' : 'Custom Marker Test', 'icon' : { custom : customIcon } },
-        { 'lat' : 33.0, 'lng' : -110.09, 'description' : '<strong>No Icon Check</strong><br/><p>Also a test for html as the description</p>' }
+        { 'lat' : 33.0, 'lng' : -110.09, 'description' : '<strong>No Icon Check</strong><br/><p>Also a test for html as the description</p>' },
+        { 'lat' : 32.10, 'lng' : -110.09, 'description' : 'Read More Marker Test', 'readmore' : { text : 'Click Me', path : '/home/some-404-page/' } }
     ]
 };
 
