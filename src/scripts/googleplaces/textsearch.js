f47ac200de400d69de8d69b1b18211427d273e08
Steev Hise <steev@detritus.net>
2017-04-25 15:12:10 -0700

    updated textsearch.js to match caching changes.

--- src/scripts/googleplaces/textsearch.js
+++ src/scripts/googleplaces/textsearch.js
@@ -22,7 +22,15 @@ import { graphql } from '../../../node_modules/graphql';
 import schema from '../../../node_modules/@freecycle/freecycle_graphql_schema';
 
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+    cache: {
+        engine: require('catbox-memory'),
+        name: 'freecycleMain',
+        partition: 'freecycle-app'
+    } }
+);
+
+server.connection({ port: process.env.PORT || 8000 });
 
 server.decorate('server', 'context', Context);   // access via server.context
 server.decorate('server', 'graphql', graphql);   // access via server.graphql
@@ -69,88 +77,96 @@ let queries = 0;
 const data = FS.readFileSync('./US-W_Groups_locations.csv', 'utf8');
 const lines = data.split('\n');
 
-// every so often, process a line
-const intervalId = setInterval(() => {
+server.start((err) => {
 
-    const line = lines.shift();
-    console.error('-------------------');
-    console.error(line);
-
-    // if no more lines
-    if (!line) {
-        clearInterval(intervalId);   // stop the loop...
-        // if we're done with the whole file, then we're done.
-        if (queries === numlines) {
-            Context.close();   // disconnect from database? Not doing this is what causes the hang at the end.
-            console.error('we are done.');
-            return;
-        }
+    if (err) {
+        console.error('server startup error', err);
     }
+    else {
+      // every so often, process a line
+        const intervalId = setInterval(() => {
 
-    numlines++;
-    const groupname = line.slice(0, line.indexOf(','));
-    let location = line.slice(line.indexOf(',') + 1);
-    location = location.replace(/"/g, '');   // get rid of extra quotes
-    //console.log(groupname + ' : ' + location);
+            const line = lines.shift();
+            console.error('-------------------');
+            console.error(line);
 
-    const parameters = {
-        query: location
-    };
-
-    // TODO: generalize this function so we can swap in other apis when we want to use others besides Google.
-    textSearch(parameters, (error, response) => {
-
-        if (error) {
-            throw error;
-        }
-        console.error('looking for ' + groupname + ' : ' + location);
-
-        if (response.results.length === 0) {
-            console.error('   0 results');
-            throw (response.error_message);     // we've probably exceeded our API quota, so bail.
-        }
-
-        // now look up the group and save the new coordinates
-        let group;
-        let id;
-        return new Group(groupname, (err, result) => {
-
-            Hoek.assert(!err, err);
-            if (result === null) {
-                console.error('couldnt find ' + groupname);
-                id = '?';
-            }
-            else {
-                group = result;
-                group.latitude = response.results[0].geometry.location.lat;
-                group.longitude = response.results[0].geometry.location.lng;
-                id = group.group_id;
-                group.id = group.group_id;
+            // if no more lines
+            if (!line) {
+                clearInterval(intervalId);   // stop the loop...
+                // if we're done with the whole file, then we're done.
+                if (queries === numlines) {
+                    Context.close();   // disconnect from database? Not doing this is what causes the hang at the end.
+                    console.error('we are done.');
+                    return;
+                }
             }
 
-            Promise.resolve(group);
+            numlines++;
+            const groupname = line.slice(0, line.indexOf(','));
+            let location = line.slice(line.indexOf(',') + 1);
+            location = location.replace(/"/g, '');   // get rid of extra quotes
 
-            console.error(group);
-            // now either way we have something to output even if we didn't find a group id.
-            console.log(Util.format('%d,%s,%s,%s', id, groupname, response.results[0].geometry.location.lat, response.results[0].geometry.location.lng));
-            console.error('group id: ' + id);
+            const parameters = {
+                query: location
+            };
 
-            // save the modified group   TODO: this is broken.
-            group.save((err,result2) => {
+            // TODO: generalize this function so we can swap in other apis when we want to use others besides Google?
+            textSearch(parameters, (error, response) => {
 
-                Hoek.assert(!err, err);
-                console.error('group ' + result2 + ' saved.');
-                Promise.resolve(result2);
+                if (error) {
+                    throw error;
+                }
+                console.error('looking for ' + groupname + ' : ' + location);
 
-            });   // we don't care if the loop goes on while the save is still happening...
+                if (response.results.length === 0) {
+                    console.error('   0 results');
+                    throw (response.error_message);     // we've probably exceeded our API quota, so bail.
+                }
 
-            queries++;
+                // now look up the group and save the new coordinates
+                let group;
+                let id;
+                return new Group(groupname, (err, result) => {
 
-        });
-    });
+                    Hoek.assert(!err, err);
+                    if (result === null) {
+                        console.error('couldnt find ' + groupname);
+                        id = '?';
+                    }
+                    else {
+                        group = result;
+                        group.latitude = response.results[0].geometry.location.lat;
+                        group.longitude = response.results[0].geometry.location.lng;
+                        id = group.group_id;
+                        group.id = group.group_id;
+                    }
 
-}, 2000);
+                    Promise.resolve(group);
 
+                    console.error(group);
+                    // now either way we have something to output even if we didn't find a group id.
+                    console.log(Util.format('%d,%s,%s,%s', id, groupname, response.results[0].geometry.location.lat, response.results[0].geometry.location.lng));
+                    console.error('group id: ' + id);
+
+                    // save the modified group
+                    group.save((err,result2) => {
+
+                        Hoek.assert(!err, err);
+                        console.error('group ' + result2 + ' saved.');
+                        Promise.resolve(result2);
+
+                    });   // we don't care if the loop goes on while the save is still happening...
+
+                    queries++;
+
+                });
+            });
+
+        }, 2000);
+
+        console.log('Server running at:', server.info.uri);
+    }
+});
 
 
 
