87d46d4cfe1cbe1c72748b0db866b0dacde8ecd4
Thomas Gray <tgray.projects@gmail.com>
2017-05-12 21:29:15 -0700

    Fix minor merge issue; update user page style.

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -251,138 +251,130 @@ module.exports = [
             id: 'pages_dashboard',
             auth: { mode: 'required' },
             plugins: {
-        // 'hapiAuthorization': {role: '1'}
-        // you don't have to have any special privs to see your own dashboard, but this is how you do it.
-      }
-    },
-    handler: function (request, reply) {
-      const inBodyAds = [
-        "one",
-        "two"
-      ];
-
-      reply.view('desktop_dash', {
-        messageSets: [
-          {
-            messageHeader: "My Replies",
-            messages: [
-              {
-                title: "Kid Clothes",
-                image: "http://lorempixel.com/250/250",
-                category: "offer",
-                type: "Reply",
-                time: '10 Minutes Ago',
-                id: 1
-              },
-              {
-                title: "Acrylic Paints",
-                image: "http://lorempixel.com/250/250",
-                category: "offer",
-                type: "Reply",
-                time: '15 Minutes Ago',
-                id: 2
-              }
-            ]
-          },
-          {
-            messageHeader: "Replies to My Posts",
-            messages: [
-              {
-                title: "Vaccum Cleaner with a very long title",
-                category: "wanted",
-                image: "http://lorempixel.com/250/250",
-                type: "PostReply",
-                time: '15 Minutes Ago',
-                notification: "7 unread replies",
-                id: 3
-              },
-              {
-                title: "Canon 560 Printer",
-                category: "lend",
-                image: "http://lorempixel.com/250/250",
-                type: "PostReply",
-                time: '15 Minutes Ago',
-                notification: "15 unread replies",
-                id: 5
-              },
-              {
-                title: "Patio Furniture",
-                category: "offer",
-                image: "http://lorempixel.com/250/250",
-                type: "PostReply",
-                time: '15 Minutes Ago',
-                notification: "1 unread reply",
-                id: 6
-              }
-            ]
-          },
-          {
-            messageHeader: "Chat With Friends",
-            messages: [
-              {
-                title: "Deron Beal",
-                image: "http://lorempixel.com/250/250",
-                type: "ChatMessage",
-                time: '1 Week Ago',
-                id: 7
-              },
-              {
-                title: "Steev Hise",
-                image: "http://lorempixel.com/250/250",
-                type: "ChatMessage",
-                time: '2 Weeks Ago',
-                id: 8
-              }
-            ]
+            // 'hapiAuthorization': {role: '1'}
+            // you don't have to have any special privs to see your own dashboard, but this is how you do it.
           }
-        ],
-        showFilterSelectors: true,
-        filterType: 'circle',
-        friends: friends,
-        inBodyAds: inBodyAds,
-        title: "Desktop Dash",
-        footerMenuItems: footerMenuItems,
-        posts: posts
-      });
-    }
-  },
-  {
-    method: 'GET',
-    path: '/pages/{page_path}',
-    config: {
-      id: 'static pages',
-      description: 'a certain static page stored in database. For example, try /pages/test',
-    }
-  },
-  {
-    method: 'GET',
-    path: '/user/{username}',
-    config: {
-      id: 'user',
-      description: "The user's profile, viewed by others.",
-      plugins: {
-        // 'hapiAuthorization': {role: '1'}
-        // you don't have to have any special privs to see your own dashboard, but this is how you do it.
-      }
-    },
-    handler: function (request, reply) {
-      const inBodyAds = [
-        "one",
-        "two"
-      ];
+        },
+        handler: function (request, reply) {
+          const inBodyAds = [
+            "one",
+            "two"
+          ];
 
-      reply.view('user', {
-        user: user,
-        showFilterSelectors: false,
-        filterType: 'circle',
-        inBodyAds: inBodyAds,
-        title: "User Profile",
-        footerMenuItems: footerMenuItems,
-        posts: posts
-      });
-    }
-  },
-    {
+          reply.view('desktop_dash', {
+            messageSets: [
+              {
+                messageHeader: "My Replies",
+                messages: [
+                  {
+                    title: "Kid Clothes",
+                    image: "http://lorempixel.com/250/250",
+                    category: "offer",
+                    type: "Reply",
+                    time: '10 Minutes Ago',
+                    id: 1
+                  },
+                  {
+                    title: "Acrylic Paints",
+                    image: "http://lorempixel.com/250/250",
+                    category: "offer",
+                    type: "Reply",
+                    time: '15 Minutes Ago',
+                    id: 2
+                  }
+                ]
+              },
+              {
+                messageHeader: "Replies to My Posts",
+                messages: [
+                  {
+                    title: "Vaccum Cleaner with a very long title",
+                    category: "wanted",
+                    image: "http://lorempixel.com/250/250",
+                    type: "PostReply",
+                    time: '15 Minutes Ago',
+                    notification: "7 unread replies",
+                    id: 3
+                  },
+                  {
+                    title: "Canon 560 Printer",
+                    category: "lend",
+                    image: "http://lorempixel.com/250/250",
+                    type: "PostReply",
+                    time: '15 Minutes Ago',
+                    notification: "15 unread replies",
+                    id: 5
+                  },
+                  {
+                    title: "Patio Furniture",
+                    category: "offer",
+                    image: "http://lorempixel.com/250/250",
+                    type: "PostReply",
+                    time: '15 Minutes Ago',
+                    notification: "1 unread reply",
+                    id: 6
+                  }
+                ]
+              },
+              {
+                messageHeader: "Chat With Friends",
+                messages: [
+                  {
+                    title: "Deron Beal",
+                    image: "http://lorempixel.com/250/250",
+                    type: "ChatMessage",
+                    time: '1 Week Ago',
+                    id: 7
+                  },
+                  {
+                    title: "Steev Hise",
+                    image: "http://lorempixel.com/250/250",
+                    type: "ChatMessage",
+                    time: '2 Weeks Ago',
+                    id: 8
+                  }
+                ]
+              }
+            ],
+            showFilterSelectors: true,
+            filterType: 'circle',
+            friends: friends,
+            inBodyAds: inBodyAds,
+            title: "Desktop Dash",
+            footerMenuItems: footerMenuItems,
+            posts: posts
+          });
+        }
+      },
+      {
+        method: 'GET',
+        path: '/user/{username}',
+        config: {
+          id: 'user',
+          description: "The user's profile, viewed by others.",
+          plugins: {
+            // 'hapiAuthorization': {role: '1'}
+            // you don't have to have any special privs to see your own dashboard, but this is how you do it.
+          }
+        },
+        handler: function (request, reply) {
+          const inBodyAds = [
+            "one",
+            "two"
+          ];
+
+          reply.view('user', {
+            user: user,
+            showFilterSelectors: false,
+            filterType: 'circle',
+            inBodyAds: inBodyAds,
+            title: "User Profile",
+            footerMenuItems: footerMenuItems,
+            posts: posts
+          });
+        }
+      },
+      {
         method: 'GET',
         path: '/pages/{pagePath}',
         config: {
@@ -569,4 +561,3 @@ module.exports = [
     }
 
 ];
-
