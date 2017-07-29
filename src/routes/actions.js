a21bd0f15b89279c629252855841dd1567118338
rwatts <ryandwatts@gmail.com>
2017-07-29 08:10:28 -0700

    cleanup

--- src/routes/actions.js
+++ src/routes/actions.js
@@ -19,25 +19,26 @@ module.exports = [
             // first determine the "command", right?
 
             // then massage the fields being input, into the object. for example:
-            console.log(request.payload.post_id);
-            const postId = request.payload.post_id;
+            const postId = Number(request.payload.post_id);
             const input = {
                 post_id: request.payload.post_id,
-                post_subject: request.payload.post_subject
+                post_subject: request.payload.post_subject,
+                post_description : request.payload.post_description
             };  // etc etc
 
             new request.server.Post(postId, (err, post) => {
-
                 Hoek.assert(!err, 'Problem getting Post!');
-                Object.assign(post, input);    // or use Hoek.merge? Hoek.clone?
-                post.save((err, post) => {
 
+                Object.assign(post, input);    // or use Hoek.merge? Hoek.clone?
+
+                post.save((err, post) => {
                     Hoek.assert(!err, 'post did not save!' + err);
                     console.log('saved post id is: ' + post.post_id);
 
                     // now return stuff to the browser (vue component?)
-                    reply.view('home/post_new', {
-                        // whatever variables you're giving back to the vue component
+                    reply.view(`/home/edit_post/${post.post_id}`, {
+                        post,
+                        title: `Edit Post : ${post.post_id}`,
                     });
                 });
             });
