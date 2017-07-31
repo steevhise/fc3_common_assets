1eb7aa800070b0cb2585cf89ee5045c3ec481ac3
rwatts <ryandwatts@gmail.com>
2017-07-30 19:31:50 -0700

    updated form component to fix class attribute and a few other items.

--- src/assets/js/components/Form.vue
+++ src/assets/js/components/Form.vue
@@ -1,72 +1,67 @@
 <template>
-    <div>
-        <div v-if="message" id="message" >{{message}}</div>
-        <form :method="method" :action="action" :classname="classname" :id="id" v-on:submit.prevent="handleSubmit" @change="getFormData" >
-            <slot></slot>
-        </form>
-    </div>
+    
+    <form :method="method" :action="action" :id="id" v-on:submit.prevent="handleSubmit" @change="getFormData">
+        <slot></slot>
+        <!-- <div v-if="message" id="message">{{message}}</div> -->
+    </form>
 </template>
 
 <script>
-    export default {
-        name : 'fc-form',
-        props: {
-            method: {
-                type: String,
-                default: () => `POST`
-            },
-            classname: {
-                type: String,
-                default: () => `btn-default btn-wide`
-            },
-            action: {
-                type: String,
-                default: () => `/actions/`
-            }
+export default {
+    name: 'fc-form',
+    props: {
+        method: {
+            type: String,
+            default: () => `POST`
         },
-        data() {
-            return {
-                message: null,
-                formData: {}
-            }
+        action: {
+            type: String,
+            default: () => `/actions/`
+        }
+    },
+    data() {
+        return {
+            message: null,
+            formData: {}
+        }
+    },
+    created() {
+        this.$on('formUpdated', (data) => {
+            this.getFormData();
+        });
+    },
+    mounted() {
+
+    },
+    methods: {
+        getFormData() {
+            $.each($(this.$el).serializeArray(), (index, element) => {
+                this.formData[element.name] = element.value;
+            });
         },
-        created() {
-          this.$on('formUpdated', (data) => {
-              this.getFormData();
-          });
-        },
-        mounted() {
-            
-        },
-        methods: {
-            getFormData() {
-                $.each($(this.$el).serializeArray(), (index, element) => {
-                    this.formData[element.name] = element.value;
+        handleSubmit(event) {
+            if (event) {
+                this.$el.submit((err, res) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(res);
+                    }
                 });
-            },
-            handleSubmit(event) {
-                if (event) {
-                    this.$el.submit((err, res)=> {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log(res);
-                        }
-                    });
-                }
             }
         }
     }
+}
 </script>
 
 <style>
-    #message {
-        background-color: #34b233;
-        text-align: center;
-        color: white;
-        vertical-align: middle;
-        margin:0 auto;
-        position: relative;
-        top:50%;
-    }
+#message {
+    background-color: #34b233;
+    text-align: center;
+    color: white;
+    vertical-align: middle;
+    margin: 0 auto;
+    position: relative;
+    top: 50%;
+}
 </style>
