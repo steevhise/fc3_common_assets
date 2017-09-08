4e4729e0c3e58599830e022aaada573d0168982b
Brittany Starr <lunarunar@gmail.com>
2017-09-08 13:15:43 -0700

    fix template bug rendering duplicate results. TODO: logic to show/hide results on click, minor styling updates

--- src/routes/home.js
+++ src/routes/home.js
@@ -247,20 +247,6 @@ const alerts = [
     }]
   },
   {
-    alert_name: 'computer',
-    alert_results: [
-      {
-        post_subject: 'Computer Monitor',
-        location: 'Tucson, AZ',
-        group: { group_name: 'Patagonia, AZ' },
-        post_date: '15 minutes ago',
-        category: 'borrow',
-        postType: { post_type_name: 'BORROW' },
-        image: 'http://lorempixel.com/350/500/city',
-        post_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis molestias, facere quisquam itaque! Labore nihil architecto nobis, repellat explicabo sit. Soluta itaque repudiandae ducimus velit aliquid, deleniti quas dicta tempora doloribus sed accusantium veniam aliquam fuga nulla iure molestiae dolore nemo unde laudantium quia! Possimus autem, nesciunt eligendi accusamus consectetur numquam. Eveniet et natus distinctio dicta reiciendis, laboriosam repellendus, in officia, accusantium saepe eos asperiores minima incidunt cupiditate sapiente doloribus id.'
-    }]
-  },
-  {
     alert_name: 'headphones',
     alert_results: []
   }
