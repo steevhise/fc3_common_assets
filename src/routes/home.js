b4874d812da8ba290539e921f54a606acb4e4410
Brittany Starr <lunarunar@gmail.com>
2017-09-18 14:16:34 -0700

    create dummy user profile data and placeholder settings page

--- src/routes/home.js
+++ src/routes/home.js
@@ -590,5 +590,49 @@ module.exports = [
                 });
             });
         }
+    },
+    {
+        method: 'GET',
+        path: '/home/settings',
+        config: {
+            id: 'home_settings',
+            description: 'Edit user profile.',
+            auth:  { mode: 'required' }
+        },
+        handler: function (request, reply) {
+            const languageOptions = ['English', 'Spanish', 'French', 'German', 'Esperanto'];
+            const notificationOptions = [
+              {
+                type: 'Email Digest',
+                description: 'Receive a summary of all new posts'
+              },
+              {
+                type: 'Per Post',
+                description: 'Receive an email for every new post'
+              },
+              {
+                type: 'Admin Only',
+                description: 'Only receive emails for new admin posts'
+              }
+            ];
+            const userData = {
+                firstName: 'Jim',
+                lastName: 'Shue',
+                tagline: 'Yes, that\'s my real name.',
+                phone: '520-555-3829',
+                languagePrefs: ['English', 'Esperanto'],
+                userGroups: ['Tucson', 'Oro Valley', 'Marana'],
+                homeGroup: { group_name: 'Tucson, AZ' },
+                defaultLocation: 'Tucson',
+                notificationPref: 1,
+                currentPic: null
+            };
+            reply.view('./home/settings', {
+                languageOptions,
+                notificationOptions,
+                userData,
+                title: 'Edit Profile'
+            });
+        }
     }
 ];
