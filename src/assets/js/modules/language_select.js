4c1cb0ad12ff8fe3aeee79cc752a3ab71426afac
Brittany Starr <lunarunar@gmail.com>
2017-09-19 12:07:56 -0700

    add in language preferences select2 and bring in current preferences

---
+++ src/assets/js/modules/language_select.js
@@ -0,0 +1,8 @@
+// setup
+document.addEventListener("DOMContentLoaded", ()=> {
+  $(".language-prefs").select2({  placeholder: "Select a language...",});
+});
+
+
+// export a constant, placeholder
+export default "LANGUAGE_SELECT";
