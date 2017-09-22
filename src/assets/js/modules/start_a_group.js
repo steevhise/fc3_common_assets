e0f14f414a5a19d1bc6538658553d19eaef1d386
Brittany Starr <lunarunar@gmail.com>
2017-09-22 15:03:02 -0700

    allow region-dropdown to reset in case user clicks more than one country (previously just appended current selection's results)

--- src/assets/js/modules/start_a_group.js
+++ src/assets/js/modules/start_a_group.js
@@ -8,8 +8,11 @@ document.addEventListener("DOMContentLoaded", ()=> {
     $('.start-group .country-dropdown').on('select2:select', function (e) {
       const {id} = e.params.data;    
       const localRegions = regions[id];
-
+      
+      // init the region dropdown
       $('.start-group .region-dropdown').select2();
+      // empty incase another country was clicked in error 
+      $('.start-group .region-dropdown').empty();
       
       localRegions.forEach(item => {
         $('.start-group .region-dropdown').append(`<option value="${item.shortCode}">${item.name}</option>`);
