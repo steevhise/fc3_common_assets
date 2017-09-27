061d47221a43921a4cb6e78f699c036a4c7f55f9
Brittany Starr <lunarunar@gmail.com>
2017-09-27 15:42:38 -0700

    flesh out location data. switch map to active view (map doesn't load properly otherwise for some reason). and create placeholder find_groups.js

---
+++ src/assets/js/modules/find_groups.js
@@ -0,0 +1,3 @@
+// attempt to get browser location
+  // if error, don't auto populate - input will come from search box
+  // otherwise, populate the list and geomap with groups within 50 miles of the user location
