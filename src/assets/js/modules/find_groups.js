3bcab40f2512908bdab15d0769574a04888640ef
Brittany Starr <lunarunar@gmail.com>
2017-10-04 18:20:12 -0700

    starting to use geocoder to transform searches into lat and long for gmaps. set map container to fixed height to allow dynamic rendering of map

--- src/assets/js/modules/find_groups.js
+++ src/assets/js/modules/find_groups.js
@@ -1,7 +1,6 @@
 export class FindGroups {
-  constructor() {
+  constructor(map) {
     this.location();
-    this.attachListeners();
   }
   
   attachListeners = () => {
@@ -10,20 +9,45 @@ export class FindGroups {
   
   location = () => {
     if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(this.locationRetrieved, this.locationError);
+      navigator.geolocation.getCurrentPosition(this.locationSuccess, this.locationError);
     } else {
       alert('Sorry, location services are not available for your device. Please enter search parameters.');
     }
   }
   
-  locationRetrieved = (position) => {
+  locationSuccess = (position) => {
     // populate the list and geomap with groups within 50 miles of the user location
     console.log(position.coords.latitude, position.coords.longitude);
   }
 
   locationError = (err) => {
     // if unable to get location, input will come from search box
-    console.log('sorry, there was an error: ', err);
+    this.attachListeners();
+    
+    const mapDiv = document.getElementById('find-groups_map');
+    const geocoder = new google.maps.Geocoder();
+    const address = "Phoenix, Arizona";
+    let map,
+        matchingLocations;
+        
+    geocoder.geocode({ address }, (results, status) => {
+      map = new google.maps.Map(mapDiv, {
+        center: results[0].geometry.location,
+        zoom: 8,
+      });
+    });
+    /*
+    <div
+      id="geomap"
+      data-geomap
+      data-geomap-markers=`${matchingLocations}`
+      data-geomap-settings='{"height":"400","width":"100%"}'>
+    </div>
+    
+    { 'lat' : 32.5, 'lng' : -110.09, 'description' : 'Tucson', 'icon' : 'group' }
+    */ 
+    $('#find-groups_map').empty();
+    $('#find-groups_map').append(/*map goes here*/);
   }
 }
 
