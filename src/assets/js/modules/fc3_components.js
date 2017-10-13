1736599c5448a9434dba9f09bbe25ac589254d7c
Steev Hise <steev@detritus.net>
2017-10-13 11:21:12 -0700

    minor wording changes.

--- src/assets/js/modules/fc3_components.js
+++ src/assets/js/modules/fc3_components.js
@@ -27,7 +27,7 @@ Vue.component('fc-login', {
                 <div class="medium-12 columns align-center-middle ">
                     <input class="btn btn-default" type="submit" value="Log in">
                     <p>
-                        Or <a href="/fb_login"><i class="fa fa-facebook"></i> Login via Facebook</a>.
+                        Or <a href="/fb_login"><i class="fa fa-facebook"></i> Sign up via Facebook</a>.
                         <!-- TODO: the link should be a swig tag: {{ path.pages_fblogin }} but it's not working at the moment-->
                     </p>
                 </div>
