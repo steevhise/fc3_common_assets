ec9ad224b364606230f31507a2908f60e311cb87
rwatts <ryandwatts@gmail.com>
2018-01-30 22:02:34 -0700

    deprecated hapi-sass plugin

--- src/plugins/frontend.js
+++ src/plugins/frontend.js
@@ -8,19 +8,6 @@ module.exports = [
     require('vision'),
     require('hapi-named-routes'),
     {
-        register: require('hapi-sass'),         // TODO: not ported to hapi 17 - replacing with webpack config
-        options: {
-            src: rel('build/scss'),
-            includePaths: rel('src/assets/scss'),
-            dest: rel('public/assets/css'),
-            force: true,
-            debug: true,
-            routePath: '/css/{file}.css',
-            outputStyle: 'nested',
-            srcExtension: 'scss'
-        }
-    },
-    {
         // TODO: this should be turned into regular module with function that returns the swig object instead of server.exposing it.
         register: require('@freecycle/common-hapi-plugins/plugins/hapi-swig-extensions'),
         options: {
