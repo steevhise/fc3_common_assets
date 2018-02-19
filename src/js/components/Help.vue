a18ce829b40a1be961541133a61d9aafc3d3efeb
Ryan D. Watts <ryandwatts@gmail.com>
2018-02-19 08:11:28 -0700

    124 [wip] added new help component slots to pass wrapping text to component.

---
+++ src/js/components/Help.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="fc-help">
+		<p>{{slot}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "fc-help",
+		data() {
+			return {
+
+			}
+		},
+		mounted() {
+			console.log("fc-help loaded");
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
