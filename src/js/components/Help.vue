87cc3a48dc69c6a0aa48af5f303c371c28a47832
Ryan D. Watts <ryandwatts@gmail.com>
2018-02-24 00:06:07 -0700

    fc-help component updates

--- src/js/components/Help.vue
+++ src/js/components/Help.vue
@@ -1,26 +1,38 @@
 <template>
-	<div id="fc-help">
-		<slot></slot>
-		<fc-modal id="2" text="Modal" type="custom" >
-			<slot name="trigger">Button</slot>
-		</fc-modal>
-	</div>
+	<span id="fc-help">
+		<i class="fa fa-question-circle fc-help-trigger" :data-open="getTarget" :style="`font-size: ${fontsize}; color: ${fontcolor}`" ></i>
+		<div class="reveal" :id="getTarget" data-reveal>
+			<h3>Freecycle Help</h3>
+			<slot>Freecycle Help Placeholder</slot>
+			<!--TODO: Add HelpScout Beacon-->
+		</div>
+	</span>
 </template>
 
 <script>
 	export default {
 		name: "fc-help",
+		props: {
+			fontsize: { default: '15px' },
+			fontcolor: { default: 'gray' }
+		},
 		data() {
 			return {
-
+				
 			}
 		},
-		mounted() {
-			console.log("fc-help loaded");
+		computed: {
+			getTarget() {
+				return `modal-${this._uid}`
+			}
 		}
 	}
 </script>
 
 <style scoped>
-
+	.fc-help-trigger {
+		position:relative;
+		bottom: 0px;
+		right: 5px;
+	}
 </style>
