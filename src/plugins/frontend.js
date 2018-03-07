f5321e71641d2a3c0eb64ab5fe845631e90cbefc
Devin Ivy <devin@bigroomstudios.com>
2018-03-07 14:28:30 -0500

    Use swig extensions as a package rather than a hapi plugin

--- src/plugins/frontend.js
+++ src/plugins/frontend.js
@@ -1,17 +1,7 @@
 const Path = require('path');
 
-// Build absolute path relative to project
-const rel = (path) => Path.resolve(__dirname, '../../', path);
-
 module.exports = () => ([
     require('inert'),
     require('vision'),
-    require('hapi-named-routes'),
-    {
-        // TODO: this should be turned into regular module with function that returns the swig object instead of server.exposing it.
-        register: require('@freecycle/common-hapi-plugins/plugins/hapi-swig-extensions'),
-        options: {
-            includeDir: rel('build/views')
-        }
-    }
+    require('hapi-named-routes')
 ]);
--- src/view-manager.js
+++ src/view-manager.js
@@ -1,14 +1,23 @@
 const Path = require('path');
+const SwigExtensions = require('@freecycle/common-hapi-plugins/modules/swig-extensions');
+
+// Build absolute path relative to project
+const rel = (path) => Path.resolve(__dirname, '../', path);
 
 module.exports = (server, options) => ({
     engines: {
-        html: server.plugins['hapi-swig-extensions'].swig
+        html: SwigExtensions.create({
+            includeDir: rel('build/views')  // For rendering common partials
+        })
     },
     isCached: !options.dev,       // Cache when not in development
     allowInsecureAccess: true,    // TODO should ensure this can be set to false
     // Note: this below is only path for local view files. Common assets get found by custom template loader!
     relativeTo: Path.resolve(__dirname, '..'), // Project root
-    path:  'src/views',
+    path: [
+        'src/views',
+        rel('build/views')                  // For rendering top-level common views
+    ],
     layoutPath: 'src/views/layout',
     context: (request) => ({
         session: request.auth.credentials,
