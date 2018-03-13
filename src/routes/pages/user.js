bfb3cc436ab6095fae80892afa22da517681b1ea
Devin Ivy <devin@bigroomstudios.com>
2018-03-13 11:35:48 -0400

    Add post endpoint for user friendship actions

--- src/routes/pages/user.js
+++ src/routes/pages/user.js
@@ -1,6 +1,7 @@
+const Joi = require('joi');
 const Boom = require('boom');
 
-module.exports = {
+module.exports = [{
     method: 'GET',
     path: '/user/{username}',
     config: {
@@ -25,7 +26,7 @@ module.exports = {
                 throw Boom.notFound('User not found');
             }
 
-            return reply({
+            return reply.view('user', {
                 title: 'User Profile',
                 showFilterSelectors: false,
                 filterType: 'circle',
@@ -39,4 +40,67 @@ module.exports = {
             });
         });
     }
-};
+},
+{
+    method: 'POST',
+    path: '/user/{identifier}/friendship',
+    config: {
+        id: 'user-friendship-action',
+        description: 'An endpoint for the current user to take action on their friendship with another user.',
+        auth: { mode: 'required' },
+        validate: {
+            payload: {
+                action: Joi.string().lowercase().required().valid([
+                    'friend',
+                    'accept',
+                    'unfriend',
+                    'reject',
+                    'block',
+                    'unblock'
+                ])
+            }
+        }
+    },
+    handler: (request, reply) => {
+
+        const { userService } = request.server;
+        const { identifier } = request.params;  // Username, id, or email address
+        const { action } = request.payload;
+        const { isAuthenticated, credentials } = request.auth;
+
+        return userService.fetchByIdentifier(identifier, 'id: user_id')
+        .then((user) => {
+
+            if (!user) {
+                throw Boom.notFound('User not found');
+            }
+
+            const friendship = {
+                friender: credentials.id,
+                friendee: user.id
+            };
+
+            if (action === 'friend' || action === 'accept') {
+                return userService.friend(friendship);
+            }
+
+            if (action === 'unfriend' || action === 'reject') {
+                return userService.unfriend(friendship);
+            }
+
+            if (action === 'block') {
+                return userService.block(friendship);
+            }
+
+            if (action === 'unblock') {
+                return userService.unblock(friendship);
+            }
+
+            throw Boom.badImplementation(`Invalid friend action "${friendAction}".`);
+        })
+        .then(() => {
+
+            return reply.redirect(request.info.referrer || '/').temporary();
+        });
+    }
+}];
