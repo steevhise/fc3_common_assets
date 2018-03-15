efc7bc0404f14f0fb8baad0bfbb7432ba9746d7b
Devin Ivy <devin@bigroomstudios.com>
2018-03-15 15:35:29 -0400

    Pass imagesURL to user service

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ exports.register = Util.callbackify((server, options) => {
     server.decorate('server', 'User', FCUser.userClassFunc(server));
     server.decorate('server', 'Group', FCGroup.groupClassFunc(server));
     server.decorate('server', 'postService', new PostService(server, { PostEntity: server.Post, UserEntity: server.User }));
-    server.decorate('server', 'userService', new UserService(server, { UserEntity: server.User }));
+    server.decorate('server', 'userService', new UserService(server, { UserEntity: server.User, imagesURL: 'https://images.freecycle.org' }));
     server.decorate('server', 'groupService', new GroupService(server, { GroupEntity: server.Group }));
     server.decorate('server', 'authService', new AuthService(server, { UserEntity: server.User }));
 
