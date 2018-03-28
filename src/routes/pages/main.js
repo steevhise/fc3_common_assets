8d6dfa71b7597a5729352a47952f8f8eaeb1c5bb
Devin Ivy <devin@bigroomstudios.com>
2018-03-28 13:50:11 -0400

    Hook-up featured groups and site stats to homepage

--- src/routes/pages/main.js
+++ src/routes/pages/main.js
@@ -9,34 +9,22 @@ module.exports = {
     },
     handler: function (request, reply) {
 
-        const { postService } = request.server;
+        const { postService, groupService, siteService } = request.server;
 
-        const localGroups = [
-            'Tuscon', 'Marana', 'Oro Valley', 'Vail', 'Sanuarita'
-        ];
-
-        const metrics = [
-            {
-                name: 'Members',
-                count: '9,073,808'
-            },
-            {
-                name: 'Local Towns',
-                count: '5,270'
-            },
-            {
-                name: 'Scams or Cost',
-                count: '0'
-            }
-        ];
-
-        return postService.fetchFeatured(3).then((posts) => {
+        return Promise.all([
+            postService.fetchFeatured(3),
+            groupService.fetchFeatured(),
+            siteService.fetchStatistics()
+        ])
+        .then(([posts, groups, statistics]) => {
 
             return reply.view('home', {
                 title: 'Freecycle',
-                posts,
-                groups: localGroups,
-                metrics
+                data: {
+                    posts,
+                    groups,
+                    statistics
+                }
             });
         });
     }
