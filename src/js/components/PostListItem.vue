a440aea161321740a4ebc7513109bc6077f6b93a
Ryan D. Watts <ryandwatts@gmail.com>
2018-04-08 09:58:33 -0700

    added post list item component to allow dynamically showing posts

--- src/js/components/PostListItem.vue
+++ src/js/components/PostListItem.vue
@@ -2,25 +2,26 @@
 	<div class="post-list-item" >
 		<div class="post-list-item-photo">
 			<img width=128 :src="post.thumb" v-if="post.thumb">
+			<fc-icon name="logo" v-else></fc-icon>
 		</div>
 		<div class="post-list-item-content">
 			<div class="post-list-item-content-header">
 				<div class="post-list-item-header-left">
 					<div class="post-list-item-category-icon">
-						<!-- {% include "./post_type.html" with { post : post } %} -->
+						<fc-icon name="chevron" :classname="`icon-chevron-${postType}`"></fc-icon>
 					</div>
 					<div class="post-list-item-header-icon">
-						<!-- {% include '../icons/map_pin.html' %} -->
+						<fc-icon name="map_pin"></fc-icon>
 						<span>{{ post.group.name }}</span>
 					</div>
 				</div>
 				<div class="post-list-item-header-right">
 					<span class="text-lighten">{{ post.date | mreldate }}</span>
-					<button :class="`btn-${post.category}`">Reply</button>
+					<button :class="`btn-${postType}`">Reply</button>
 				</div>
 			</div>
 			<div class="post-list-item-content-description">
-				<h4><a :href="path.posts_detail + post.post_id">{{ post.subject }}</a></h4>
+				<h4><a :href="path.posts_detail + post.id">{{ post.subject }}</a></h4>
 				<p class="paragraph-small" v-html="post.description"></p>
 			</div>
 		</div>
@@ -30,9 +31,16 @@
 <script>
 	export default {
 		name: 'fc-post-list-item',
-		props: ['post', 'path'],
-		mounted() {
-			console.log(this.post, this.path);
+		props: ['item', 'path', 'index'],
+		data() {
+			return {
+				post: this.item
+			}
+		},
+		computed: {
+			postType() {
+				return this.post.type.name.toLowerCase();
+			}
 		}
 	}
 </script>
