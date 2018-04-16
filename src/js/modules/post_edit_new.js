28d34850f99bc739f055d451d9f5ef99330d9080
Devin Ivy <devin@bigroomstudios.com>
2018-04-16 17:50:47 -0400

    Refactor post type button bar

--- src/js/modules/post_edit_new.js
+++ src/js/modules/post_edit_new.js
@@ -4,31 +4,44 @@
 /**
  * A simple method that grabs all post type buttons,
  * and allows us to determine whether or not it is active.
- * 
+ *
  * @method PostEditNew
  */
 const PostEditNew = () => {
-   const $postTypes = $('[data-post-buttongroup] [data-post-typebutton]');
-   const $root = $('[data-post-buttongroup]');
 
-   $postTypes.each(function(idx, item) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            clearActives($postTypes);
-            $(this).toggleClass('active');
-        });
-    });
+   const roots = $('[data-post-buttongroup]');
+
+   roots.each((i, root) => {
+
+      const input = $(root).find('input[type=hidden]');
+      const options = $(root).find('[data-post-typebutton]');
+
+      options.each((j, option) => {
+
+         $(option).on('click', (e) => {
+
+           e.preventDefault();
+
+           input.val($(option).data('value'));
+
+           clearActives(options);
+           $(option).toggleClass('active');
+         });
+      });
+   });
 };
 
 /**
  * This method is used to clear the active classes
- * 
+ *
  * @method clearActives
  * @param {Array} items an array of items.
  */
 const clearActives = (items) => {
-    $(items).each(function() {
-        $(this).removeClass('active');
+
+    items.each((i, item) => {
+
+        $(item).removeClass('active');
     });
 };
 
