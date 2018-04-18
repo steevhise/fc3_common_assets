57f009caf6e57da8f7f282cd607e2974f9715338
Zack McCartney <zack@bigroomstudios.com>
2018-04-18 16:28:23 -0400

    First pass on image uploading backend for posts

--- src/routes/home/new-post.js
+++ src/routes/home/new-post.js
@@ -8,9 +8,30 @@ module.exports = {
         id: 'home_post_new',
         description: 'Create a new post.',
         auth: { mode: 'required' },
+        payload: {
+            // processes any input from the images field into Buffers representing the image files in memory
+            multipart: { output: 'data' },
+            maxBytes: 1048576 * postService.MAX_POST_IMAGES, // 1mb per image
+            failAction: (request, reply, error) => {
+
+                // TODO ensure error is about image size and not some other parsing error
+
+                request.app.formValidation = request.app.formValidation || [];
+                request.app.formValidation = [{
+                    type: 'form',
+                    path: 'image',
+                    message: 'The maximum individual image size is 1mb'
+                }];
+
+                return reply.continue();
+            }
+        },
         validate: {
             failAction: RouteHelpers.formFailAction,
-            payload: {
+            // We have to explicitly wrap the entire schema in Joi.object
+            // so we can append the empty rule at the end, which covers the case where the payload processing fails, in which
+            // case the payload is null and would otherwise trigger the validation fail action with an opaque, Joi-internals error message
+            payload: Joi.object({
                 subject: Joi.string()
                     .required()
                     .label('Subject'),
@@ -39,8 +60,29 @@ module.exports = {
                     .label('Town'),
                 location: Joi.string()
                     .empty('')
-                    .label('Crossroads')
-            },
+                    .label('Crossroads'),
+                images: Joi.array()
+                    // Handles the case of 1 valid upload
+                    .single()
+                    .items(
+                        Joi.binary().min(1)
+                    )
+                    .max(postService.MAX_POST_IMAGES)
+                    // one of the empty schemas is an empty object because the route's configured multipart processing outputs an empty object
+                    // when no images are sent in the payload AND the form's encoding is multipart/form-data (enctype="multipart/form-data")(which it is and needs to be to upload image files)
+                    // If the form is not encoded thusly, the output of an empty images input is an empty Buffer. Not sure what this means, just felt it was odd/worth noting :)
+                    .empty({}, '', null)
+                    .when('type', {
+                        // People can post images only of what they have (no hypotheticals, only reals)
+                        is: Joi.any().required().valid([
+                            postService.BORROW,
+                            postService.WANTED
+                        ]),
+                        then: Joi.forbidden()
+                    })
+                    .label('Post images')
+            })
+            .empty(null),
             options: {
                 abortEarly: false,
                 language: {
@@ -62,13 +104,13 @@ module.exports = {
                     return reply.continue();
                 }
 
-                const { postService } = request.server;
+                const { postService, userService } = request.server;
                 const { id: userId } = request.auth.credentials;
                 const { validation, ...post } = request.payload;
 
                 return Promise.resolve()
                     .then(() => postService.create(userId, post))
-                    .then((postId) => reply.redirect(`/post/${postId}`).takeover())
+                    .then((postId) => reply.redirect(`/posts/${postId}`).takeover())
                     .catch((err) => {
 
                         request.app.formValidation = request.app.formValidation || [];
@@ -78,7 +120,18 @@ module.exports = {
                             request.app.formValidation.push({
                                 type: 'form',
                                 path: 'town',
-                                message: 'Sorry, you can post to that town because you\'re not a member.'
+                                message: 'Sorry, you can\'t post to that town because you\'re not a member.'
+                            });
+
+                            return reply.continue();
+                        }
+
+                        if (err instanceof postService.BadImageFormatError) {
+
+                            request.app.formValidation.push({
+                                type: 'form',
+                                path: 'image',
+                                message: 'Image is an unsupported image format.  Must be .jpg or .png.'
                             });
 
                             return reply.continue();
