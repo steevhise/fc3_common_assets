b87ff792aab93b85021964616009e2293087e803
Zack McCartney <zack@bigroomstudios.com>
2018-04-24 18:04:16 -0400

    Basic styling on image uploading component

---
+++ src/js/modules/image_uploader.js
@@ -0,0 +1,37 @@
+// Add up to 3 images
+// Delete any uploaded image
+// Fill in uploader w/ any previously uploaded images
+
+
+
+
+
+// TESTS
+
+/**
+// I can upload 3 images to a post
+1. Fill in form, add 3 images
+2. Try to add 4th --> fail
+3. Submit
+
+EXPECT
+- Land on post detail, 3 images displayed
+
+
+
+// I can upload a post w/ no images
+1. Fill in form
+2. Submit
+EXPECT
+OK
+
+
+// I can edit the images uploaded to a post
+1. Edit post
+2. Replace at least 1 image
+3. Submit
+
+EXPECT
+- Land on post detail, 2 same images, 1 new
+
+**/
