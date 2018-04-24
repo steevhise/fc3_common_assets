ea3f3fffdcbea9a43bbd7407210af66a991ee71a
Devin Ivy <devin@bigroomstudios.com>
2018-04-24 15:39:49 -0400

    Further post edit service integration

--- src/routes/home/edit-post.js
+++ src/routes/home/edit-post.js
@@ -39,57 +39,31 @@ module.exports = {
                     .label('Description'),
                 type: Joi.number()
                     .valid([
-                        // TODO Need to add TAKEN and RECEIVED here?
-                        postService.OFFER,
-                        postService.WANTED,
-                        postService.BORROW,
-                        postService.LEND
+                        postService.TAKEN,
+                        postService.RECEIVED
                     ])
-                    .required()
                     .label('Type'),
-                town: Joi.number()
-                    .integer().min(1)
-                    .empty(null, '')
-                    .when('type', {
-                        // TODO Require town if not forbidden
-                        is: Joi.number().required().valid([
-                            postService.BORROW,
-                            postService.LEND
-                        ]),
-                        then: Joi.forbidden()
-                    })
-                    .label('Town'),
-                // TODO Need to require? Fails if not supplied b/c
-                // Post.entity expects this to exist, I think...?
                 location: Joi.string()
                     .empty('')
                     .label('Crossroads'),
                 images: Joi.array()
-                        // Handles the case of 1 valid upload
-                        .single()
-                        .items(
-                            Joi.binary().min(1)
-                        )
-                        .max(postService.MAX_POST_IMAGES)
-                        // one of the empty schemas is an empty object because the route's configured multipart processing outputs an empty object
-                        // when no images are sent in the payload AND the form's encoding is multipart/form-data (enctype="multipart/form-data")(which it is and needs to be to upload image files)
-                        // If the form is not encoded thusly, the output of an empty images input is an empty Buffer. Not sure what this means, just felt it was odd/worth noting :)
-                        .empty({}, '', null)
-                        .when('type', {
-                            // People can post images only of what they have (no hypotheticals, only reals)
-                            is: Joi.any().required().valid([
-                                postService.BORROW,
-                                postService.WANTED
-                            ]),
-                            then: Joi.forbidden()
-                        })
-                        .label('Post images'),
+                    // Handles the case of 1 valid upload
+                    .single()
+                    .items(
+                        Joi.binary().min(1)
+                    )
+                    .max(postService.MAX_POST_IMAGES)
+                    // one of the empty schemas is an empty object because the route's configured multipart processing outputs an empty object
+                    // when no images are sent in the payload AND the form's encoding is multipart/form-data (enctype="multipart/form-data") (which it is and needs to be to upload image files)
+                    // If the form is not encoded thusly, the output of an empty images input is an empty Buffer. Not sure what this means, just felt it was odd/worth noting :)
+                    .empty({}, '', null)        // TODO disallow images for certain types?
+                    .label('Post images'),
                 tags: Joi.array()
                     .single()
                     .items(Joi.number().integer().min(1))
                     .label('Tags')
             })
-            .empty(null),
+                .empty(null),
             options: {
                 abortEarly: false,
                 language: {
@@ -98,76 +72,77 @@ module.exports = {
             }
         },
         pre: [
-            {
-                // 'post' is the key on request.pre to which the return value of method is assigned
-                // return value is only ever the post data loaded via fetchByIdentifier (just GET or invalid POST requests)
-                assign: 'post',
-                method: (request, reply) => {
+            (request, reply) => {
 
-                    const { id: userId } = request.auth.credentials;
-                    const { postId } = request.params;
+                if (!request.payload) {
+                    return reply.continue();
+                }
 
-                    return postService.fetchByIdentifier(postId, { viewerId: userId })
-                    .then((post) => {
+                // Handle a POST
 
-                        // Only post author can edit a post
-                        if (userId !== post.user.id) {
-                            request.log('authorization', 'post author id is ' + post.user.id + ' but current user is ' + userId);
-                            const path = request.route.path.replace('{postId}', postId);
+                // Failed validation
+                if (request.payload.validation) {
+                    return reply.continue();
+                }
 
-                            reply.state('redirectedError', {
-                                message: 'You\'re not allowed to edit other users\' posts',
-                                path,
-                                type: 'postEditForbidden'
-                            });
-                            return reply.redirect(`/posts/${postId}`).temporary().takeover();
-                        }
+                const { postId } = request.params;
+                const { id: userId } = request.auth.credentials;
 
-                        // Handle a GET OR failed validation (in either case, we're onto the handler)
-                        if (!request.payload || request.payload.validation) {
-                            // In prerequisites, reply interface with a value assigns the value
-                            // to request.pre, does NOT send value to the client (inconsistency explained here: https://github.com/hapijs/hapi/blob/v16/API.md#route-prerequisites)
-                            // TODO What key of request.pre is this assigned to?
-                            return reply(post);
-                        }
+                return postService.update(postId, userId, request.payload)
+                .then(() => {
 
-                        // Handle a POST
-                        // TODO Need to kick off w/ Promise.resolve()?
-                        // TODO Safe to pass payload directly?
-                        return Promise.resolve()
-                        .then(() => postService.update(userId, postId, request.payload))
-                        .then((postId) => reply.redirect(`/posts/${postId}`).temporary().takeover())
-                        .catch((err) => {
+                    return reply.redirect(`/posts/${postId}`).temporary().takeover();
+                });
+            }
+        ],
+        ext: {
+            onPreResponse: {
+                method(request, reply) {
 
-                            // TODO Actually handle cases, ya turkey
-                            return reply(err);
+                    if (!request.response.isBoom) {
+                        return reply.continue();
+                    }
+
+                    const err = request.response;
+                    const { postService } = request.server;
+
+                    /*
+                    TODO handle each error from update()
+                    // Only post author can edit a post
+                    if (userId !== post.user.id) {
+                        request.log('authorization', 'post author id is ' + post.user.id + ' but current user is ' + userId);
+                        const path = request.route.path.replace('{postId}', postId);
+
+                        reply.state('redirectedError', {
+                            message: 'You\'re not allowed to edit other users\' posts',
+                            path,
+                            type: 'postEditForbidden'
                         });
-                    });
+                        return reply.redirect(`/posts/${postId}`).temporary().takeover();
+                    }
+                    */
+
+                    return reply.continue();
                 }
             }
-        ]
+        }
     }),
     handler: (request, reply) => {
 
-        // GOAL: Delivering post data, DATA OF THE CURRENT POST, to the frontend
-        // TODO On error, revert to current val
-        // TODO On error, send back ok values
-
-        const { userService, postService } = request.server;
+        const { postService } = request.server;
         const { id: userId } = request.auth.credentials;
-        const { post } = request.pre;
+        const { postId } = request.params;
 
         return Promise.all([
-            postService.fetchTags(),
-            userService.fetchTownMemberships(userId)
+            postService.fetchForUpdate(postId, userId),
+            postService.fetchTags()
         ])
-        .then(([tags, towns]) => {
+        .then(([post, tags]) => {
 
             reply.view('home/post_edit', {
                 data: {
+                    title: 'Edit Post',
                     post,
-                    title: `Edit Post : ${post.id}`,
-                    towns,
                     tags
                 },
                 inBodyAds: [
