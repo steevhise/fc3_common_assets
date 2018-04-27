11087ff5805723dd4b5a2ed51d8e7137a36f17be
Devin Ivy <devin@bigroomstudios.com>
2018-04-27 16:03:41 -0400

    Refactor /find-towns to use service functionality from P4/FindTowns branch

--- src/routes/pages/find-towns.js
+++ src/routes/pages/find-towns.js
@@ -1,7 +1,3 @@
-const Gql = require('@freecycle/common-hapi-plugins/modules/graphql-wrapper');
-const Countries = require('../../assets/js/modules/countries');
-const Regions = require('../../assets/js/modules/regions');
-
 const internals = {};
 
 module.exports = {
@@ -12,20 +8,10 @@ module.exports = {
         description: 'Search for towns.',
         pre: [
             {
-                assign: 'groups',
+                assign: 'groupsWithLocationInfo',
                 method: (request, reply) => {
 
-                    return internals.fetchAllGroupLocations({
-                        where: {
-                            deleted: false,
-                            nga_approved: true,
-                            $or: {
-                                latitude: { $ne: 0 },
-                                longitude: { $ne: 0 }
-                            }
-                        }
-                    })
-                    .then(({ groups }) => groups)
+                    return request.server.groupService.fetchAllWithLocationInfo()
                     .then(reply, reply);
                 }
             }
@@ -36,75 +22,29 @@ module.exports = {
     },
     handler: function (request, reply) {
 
-        const groupList = [
-            {
-                name: 'Chandler',
-                state: 'AZ',
-                distance: 0
-            },
-            {
-                name: 'Ahwatukee',
-                state: 'AZ',
-                distance: 15
-            },
-            {
-                name: 'Peoria',
-                state: 'AZ',
-                distance: 40
-            },
-            {
-                name: 'Tucson',
-                state: 'AZ',
-                distance: 100
-            },
-            {
-                name: 'Dewey-Humboldt',
-                state: 'AZ',
-                distance: 107
-            },
-            {
-                name: 'Lake Havasu City',
-                state: 'AZ',
-                distance: 225
-            }
-        ];
-
-        const groupMap = {
-            settings: {
-                height: '400',
-                width: '100%'
-            },
-            markers: [
-                { lat : 33.306329, lng : -111.840852, description : 'Chandler', icon: 'group' },
-                { lat : 33.342573, lng : -111.983492, description : 'Ahwaktukee', icon: 'user' },
-                { lat : 33.581569, lng : -112.240584, description : 'Peoria', icon: 'post' },
-                { lat : 32.218476, lng : -110.905825, description : 'Tucson', icon: 'post' },
-                { lat : 34.505731, lng : -112.242147, description : 'Dewey-Humboldt', icon: 'post' },
-                { lat : 34.482473, lng : -114.322985, description : 'Lake Havasu City', icon: 'post' }
-            ]
-        };
-
         // Handles the case where a user supplies an invalid group name to /town/{uniqueName},
         // which we handle by redirecting here with a cookie (redirectedError) describing the error
         const errors = [];
+
         if (request.state.redirectedError) {
+
             const { redirectedError } = request.state;
+
             if (redirectedError.type === 'groupNotFound') {
                 errors.push({
                     message: redirectedError.message
                 });
             }
+
             // Clean up the error cookie to ensure no fraudulent errors on subsequent visits in the session
             reply.unstate('redirectedError');
         }
 
         reply.view('find-groups', {
             title: 'Find Towns',
-            data: request.pre,
-            groupList,
-            countries: Countries,
-            regions: Regions,
-            geomap: groupMap,
+            data: {
+                ...request.pre.groupsWithLocationInfo
+            },
             inBodyAds: [
                 'one',
                 'two'
@@ -113,14 +53,3 @@ module.exports = {
         });
     }
 };
-
-internals.fetchAllGroupLocations = Gql.buildQuery(`
-    query ($where: SequelizeJSON!) {
-        groups(where: $where) {
-            id: group_id
-            name: group_name
-            latitude
-            longitude
-        }
-    }
-`);
