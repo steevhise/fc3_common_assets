d6e1f7ae988b824a007cc07aa32c81ec602a5f41
Ryan Watts <ryandwatts@gmail.com>
2018-04-29 01:52:41 -0700

    use image instead of thumbs

--- src/js/components/PostListItem.vue
+++ src/js/components/PostListItem.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="post-list-item" >
 		<div class="post-list-item-photo">
-			<img width="128" :src="post.thumb" v-if="post.thumb">
+			<img :src="post.image" v-if="post.image">
 			<fc-icon name="logo" v-else></fc-icon>
 		</div>
 		<div class="post-list-item-content">
