e508ea41dc1219cdaa2d162493ec696fa3b88d22
Zack McCartney <zack@bigroomstudios.com>
2018-05-17 16:48:50 -0400

    Complete initial pass on town info page

--- src/routes/towns/info.js
+++ src/routes/towns/info.js
@@ -1,48 +1,65 @@
-const Mocks = require('./helpers/mocks');
+const Joi = require('joi');
+const RouteHelpers = require('../helpers');
 
 module.exports = {
     method: 'GET',
-    path: '/town/info/{uniqueName}',
+    path: '/town/{uniqueName}/info',
     config: {
         id: 'groups_info',
-        description: 'The named town\'s information.'
+        description: 'The named town\'s information.',
+        validate: {
+            params: {
+                // NOTE Joi.number needs to be the first alternative tried, so we cast any numeric ids (i.e. group_id), which come in as strings, to numbers
+                // Otherwise, a string representation of a number would hit our fetchByIdentifier call, which would interpret the string as the yahoo_group_name
+                uniqueName: Joi.alternatives([Joi.number().integer(), Joi.string()])
+            },
+            failAction: (request, reply, source, error) => {
+
+                if (!request.params.uniqueName) {
+                    return reply.redirect('/find-towns').temporary().takeover();
+                }
+
+                throw error;
+            }
+        },
+        pre: [
+            RouteHelpers.groupDetailPre
+        ]
     },
     handler: function (request, reply) {
 
-        const freecycle = {
-            copyright:    'The official TFN copyright notice.',
-            disclaimer:   'The official TFN disclaimer.',
-            logo:         'The official TFN logo  (bare <img> tag with width and height attributes)',
-            num_groups:   'The number of official freecycle towns.',
-            num_members:  'The total number of members in all towns.'
-        };
+        const { siteService } = request.server;
+        const { group, membershipChecks } = request.pre.groupDetail;
 
-        const descriptionTokens = {
-            '%%copyright': freecycle.copyright,
-            '%%custom_logo': Mocks.group.logo,
-            '%%disclaimer': freecycle.disclaimer,
-            '%%freecycle_logo': freecycle.logo,
-            '%%group_name': Mocks.group.group_name,
-            '%%num_groups': freecycle.num_groups,
-            '%%num_members': Mocks.group.num_members,
-            '%%total_num_members': freecycle.num_members,
-            '%%yahoogroup_link': '<a href="' + Mocks.group.yahoo_group_name + '">' + Mocks.group.yahoo_group_name + '</a>"'
-        };
+        return siteService.fetchStatistics()
+        .then(({ userCount, townCount }) => {
 
-        const group = Object.assign({}, Mocks.group);
+            const descriptionTokens = {
+                '%%copyright': 'The official TFN copyright notice.', // TODO Where do I get this?
+                '%%disclaimer': 'The official TFN disclaimer.', // TODO Where do I get this?
+                '%%freecycle_logo': '<img src="/images/logo.png" />', // TODO Need to set a width and height here?
+                '%%group_name': group.group_name,
+                '%%num_groups': townCount,
+                '%%num_members': group.num_members,
+                '%%total_num_members': userCount,
+                '%%yahoogroup_link': `<a href="/town/${group.yahoo_group_name}">${group.yahoo_group_name}</a>"`
+            };
 
-        group.description = group.description.replace(/%%\w+/g, (all) => {
+            group.description = group.description.replace(/%%\w+/g, (match) => {
 
-            return descriptionTokens[all] || all;
-        });
+                return descriptionTokens[match] || match;
+            });
 
-        reply.view('groups/info', {
-            group,
-            freecycle,
-            inBodyAds: [
-                'one',
-                'two'
-            ]
+            reply.view('groups/group', {
+                data: {
+                    group,
+                    ...membershipChecks
+                },
+                inBodyAds: [
+                    'one',
+                    'two'
+                ]
+            });
         });
     }
 };
