76aadeed4277132fe649d6aa4f0dc6f87030f75d
Ryan Watts <ryandwatts@gmail.com>
2018-06-04 08:59:41 -0400

    IMS Messages Component

---
+++ src/js/components/Messages.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="fc-messages">
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name : 'fc-messages',
+		props: {
+			
+		},
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
