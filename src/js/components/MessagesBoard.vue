79cb690e87781e65f5691d91dce9994d1316d4c5
Ryan Watts <ryandwatts@gmail.com>
2018-06-04 08:59:54 -0400

    IMS Messages Board Component

---
+++ src/js/components/MessagesBoard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="fc-messages-board">
+	
+	</div>
+</template>
+
+<script>
+	export default {
+		name : 'fc-messages-board',
+		props: {
+			
+		},
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
