9b61b72a68c8c5201f07e2a63d871a48c4e79ee7
Ryan Watts <ryandwatts@gmail.com>
2018-06-04 09:01:18 -0400

    IMS Messages Notifier Component

---
+++ src/js/components/MessagesNotifier.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="fc-messages-notifier">
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'fc-messages-notifier',
+		props: {
+			
+		},
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
