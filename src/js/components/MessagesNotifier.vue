773a7832696526a8ab5ada217d2769886451a16a
Ryan Watts <ryandwatts@gmail.com>
2018-06-14 07:42:05 -0700

    stubbed unread count method for front end.

--- src/js/components/MessagesNotifier.vue
+++ src/js/components/MessagesNotifier.vue
@@ -1,18 +1,26 @@
 <template>
-	<div id="fc-messages-notifier">
-		
-	</div>
+	<span id="fc-messages-notifier">{{getUnreadCount()}}</span>
 </template>
 
 <script>
 	export default {
 		name: 'fc-messages-notifier',
 		props: {
-			
+			userId: null
 		},
 		data() {
 			return {
 				
+			}
+		},
+		methods: {
+			/**
+			 * TODO: This is a stub that will eventually get the unread count from the messages endpoint.
+			 * @returns {number}
+			 */
+			getUnreadCount() {
+
+				return 12;
 			}
 		}
 	}
