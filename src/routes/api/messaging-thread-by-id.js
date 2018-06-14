87330460e9a871534124195dbd95a223fc4b0e28
Zack McCartney <zack@bigroomstudios.com>
2018-06-14 16:00:52 -0400

    Added send and fetchThread endpoints, patched topic by id

--- src/routes/api/messaging-thread-by-id.js
+++ src/routes/api/messaging-thread-by-id.js
@@ -1,80 +1,81 @@
+const Boom = require('boom');
 const Joi = require('joi');
 
 const internals = {};
 
 module.exports = {
     method: 'get',
-    path: '/api/messaging/threads/{identifier}',
+    path: '/api/messaging/threads/{identifier*}',
     config: {
         tags: ['api'],
         auth: { mode: 'required' },
         validate: {
-            params: {
-                identifier: Joi.any()
+            // Allows route to accept requests of both /threads/5 (5 = thread id) and /threads/group/{groupId} or /threads/post/{postId}/{otherUsersId}
+            params: (value, options, next) => {
+
+                const threadParams = value.identifier.split('/');
+
+                if (threadParams.length === 1) {
+                    return Joi.validate(threadParams[0], Joi.number().integer(), (err, val) => {
+
+                        if (err) {
+                            return next(err);
+                        }
+
+                        return next(null, val);
+                    });
+                }
+
+                const threadIdentifier = {
+                    type: null,
+                    id: null,
+                    userId: null
+                };
+
+                Object.keys(threadIdentifier).forEach((k, i) => {
+
+                    threadIdentifier[k] = threadParams[i];
+                });
+
+                return Joi.validate(threadIdentifier, Joi.object({
+                    type: Joi.string().valid('friend', 'group', 'post', 'system').required(),
+                    // Can't be integer, post ids may exceed limit of Joi's definition of integer
+                    id: Joi.when('type', {
+                        is: Joi.string().not('system'), // TODO Not right, would mean request of /threads/system i.e. ambiguous for all sys requests
+                        then: Joi.number().required()   // Are system threads identifiable by userId and type? I don't think so? What else would we need?
+                    }),                                 // How do we uniquely identify system-type messages other than by thread id? (do we need to?)
+                    userId: Joi.when('type', {
+                        is: 'post',
+                        then: Joi.number().required()
+                    })
+                }), (err, val) => {
+
+                    if (err) {
+                        return next(err);
+                    }
+
+                    return next(null, val);
+                });
             }
         },
         handler(request, reply) {
 
-            const { id } = request.auth.credentials;
-            const messages = [...(new Array(100))].map(() => internals.message(id));
+            const { messagingService } = request.server;
 
-            return reply({
-                id: request.params.identifier,
-                topic: {
-                    type: 'post-mine',
-                    post: {
-                        id: 50268218,
-                        subject: 'King size divan base',
-                        type: {
-                            typeId: 1,
-                            const: 'FC_POST_OFFER',
-                            name: 'OFFER'
-                        }
-                    }
-                },
-                user: { id: 1, username: 'deronbeal' },
-                unreadCount: 3,
-                messages: internals.sortByCreatedAt(messages)
-                    .map(internals.setId)
-                    .map(internals.markThreeRead)
+            const { id: userId } = request.auth.credentials;
+            const threadIdentifier = request.params;
+
+            return Promise.resolve()
+            .then(() => messagingService.fetchThread(userId, threadIdentifier))
+            .then((thread) => reply(thread))
+            .catch((err) => {
+
+                if (messagingService.serviceErrors.some((errClass) => err instanceof errClass)) {
+                    throw Boom.boomify(err, { statusCode: 404, message: err.constructor.name });
+                }
+
+                throw err;
             });
         }
     }
-};
-
-internals.message = (currentUserId) => {
-
-    const now = Date.now();
-    const randomTimeInPastMonth = Math.round(Math.random() * 1000 * 60 * 60 * 24 * 30);
-
-    return {
-        id: null,
-        createdAt: new Date(now - randomTimeInPastMonth),
-        userId: internals.getRandom([currentUserId, 1]), // The current user and Deron chat
-        read: true,
-        body: internals.getRandom([
-            'So, do you want this huge divan base?',
-            'Hmmmm, I can\'t really decide if I want this divan base',
-            'That\'s okay, you have a little bit of time to decide',
-            'Thanks for all the time for me to make this big decision'
-        ])
-    };
-};
-
-internals.sortByCreatedAt = (arr) => arr.sort((m1, m2) => m1.createdAt.getTime() - m2.createdAt.getTime());
-
-internals.setId = (message, index) => ({
-    ...message, id: 89732466 + index
-});
-
-internals.markThreeRead = (message, index, arr) => ({
-    ...message,
-    read: (index < arr.length - 3)
-});
-
-internals.getRandom = (arr) => {
-
-    const randomIndex = Math.floor(Math.random() * arr.length);
-
-    return arr[randomIndex];
 };
