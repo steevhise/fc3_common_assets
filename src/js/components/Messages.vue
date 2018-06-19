897e830611e9a72e6709a089ecd0f856ba20db06
Devin Ivy <devin@bigroomstudios.com>
2018-06-19 16:52:24 -0400

    Break swig partial into vue messaging components

--- src/js/components/Messages.vue
+++ src/js/components/Messages.vue
@@ -1,19 +1,57 @@
 <template>
-	<div id="fc-messages">
-		
+	<div>
+		<div v-for="message in messages" class="message-list-item-details-chat-window">
+			<div class="message-list-item-details-chat-message" v-bind:class="{ 'message-from-self': message.userId === me.id }">
+				<p class="chat-message-from">
+					<span class="chat-message-avatar" v-bind:style="{ background: color(message.userId) }"></span>
+					{{user(message.userId).username}}
+				</p>
+				<p class="chat-message-message">{{message.body}}</p>
+				<p class="chat-message-time">{{ago(message.updatedAt)}}</p>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+	import moment from 'moment';
+
 	export default {
-		name : 'fc-messages',
+		name: 'fc-messages',
 		props: {
-			
+			messages: Array,
+			me: Object,
+			you: Object
 		},
 		data() {
 			return {
-				
+			}
+		},
+		methods: {
+			ago: (time) => moment(time).fromNow(),
+			color: (id) => colors[id % colors.length],
+			user(userId) {
+
+				if (userId === this.me.id) {
+					return this.me;
+				}
+				else if (userId === this.you.id) {
+					return this.you;
+				}
+
+				return null;
 			}
 		}
 	}
+
+	// TODO dry-up with MessagesBoard component
+	const colors = [
+		'rgb(186,104,200)',
+		'rgb(255,167,38)',
+		'rgb(175,180,43)',
+		'rgb(38,166,154)',
+		'rgb(236,64,122)',
+		'rgb(255,213,79)',
+		'rgb(126,87,194)'
+	]
 </script>
--- src/js/components/MessagesBoard.vue
+++ src/js/components/MessagesBoard.vue
@@ -1,19 +1,66 @@
 <template>
-	<div id="fc-messages-board">
-	
+	<div class="message-list-item-details">
+		<div class="message-list-item-details-sidebar">
+			<ul class="message-list-item-details-participants">
+				<li v-for="thread in threads" class="message-list-item-details-participant">
+					<span class="chat-message-avatar" v-bind:style="{ background: color(thread.user.id) }"></span>
+					{{thread.user.username}}
+					<span class="unread-amount">{{thread.unreadCount || null}}</span>
+				</li>
+			</ul>
+		</div>
+		<div class="message-list-item-details-chat">
+			<fc-messages v-bind:messages="messages" v-bind:me="me" v-bind:you="you"/>
+			<div class="message-list-item-details-chat-form">
+				<form>
+					<textarea placeholder="Write a Message.."></textarea>
+					<button class="btn-default">Send</button>
+				</form>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name : 'fc-messages-board',
+		name: 'fc-messages-board',
 		props: {
-			
+			threads: {
+				type: Array,
+				default: [ // TODO remove
+					{ id: 10, user: { id: 42, username: 'devinivy' }, unreadCount: 0 },
+					{ id: 11, user: { id: 69, username: 'zack' }, unreadCount: 10 }
+				]
+			},
+			messages: {
+				type: Array,
+				default: [ // TODO remove
+					{ id: 10, userId: 42, body: 'Hey!', updatedAt: '2018-06-12T19:18:46.030Z' },
+					{ id: 11, userId: 69, body: 'What is goin on?!', updatedAt: '2018-06-17T19:18:46.030Z' },
+					{ id: 12, userId: 42, body: 'Nothing is going on!', updatedAt: '2018-06-18T19:18:46.030Z' },
+					{ id: 13, userId: 69, body: 'Hey!', updatedAt: '2018-06-19T19:18:46.030Z' }
+				]
+			}
 		},
 		data() {
-			return {
-				
+			return {	// TODO remove
+				me: { id: 42, username: 'devinivy' },
+				you: { id: 69, username: 'zack' }
 			}
+		},
+		methods: {
+			color: (id) => colors[id % colors.length],
 		}
 	}
+
+	// TODO dry-up with Messages component
+	const colors = [
+		'rgb(186,104,200)',
+		'rgb(255,167,38)',
+		'rgb(175,180,43)',
+		'rgb(38,166,154)',
+		'rgb(236,64,122)',
+		'rgb(255,213,79)',
+		'rgb(126,87,194)'
+	]
 </script>
