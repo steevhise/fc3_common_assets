7ffb353e8f336853857ecc3e482257beccfe5c2e
Ryan Watts <ryandwatts@gmail.com>
2018-06-19 03:31:15 -0700

    create messaging store

---
+++ src/assets/js/vuex_stores/messaging.js
@@ -0,0 +1,27 @@
+/**
+ * This file contains the Vuex Store for the Internal Messaging System
+ * @see src/assets/js/modules/fc3_components.js to register a new store.
+ */
+
+const state = {
+	example: null
+};
+
+const mutations = {
+	
+};
+
+const actions = {
+	
+};
+
+const getters = {
+	
+};
+
+export const MessagingStore = {
+	state,
+	mutations,
+	actions,
+	getters
+};
