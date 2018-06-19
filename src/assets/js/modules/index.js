76f064961eb375b28f4fb412e16311ef32f96483
Ryan Watts <ryandwatts@gmail.com>
2018-06-19 03:31:50 -0700

    register Main's store as a module

--- src/assets/js/modules/index.js
+++ src/assets/js/modules/index.js
@@ -3,7 +3,7 @@ export {default as LanguageSelect} from "./language_select";
 export {default as HomeGroup} from "./home_group";
 export {default as DefaultLocation} from "./default_location";
 export {default as NotificationPrefs} from "./notification_prefs";
-export {MainVue} from './fc3_components';
+export {MainVue,MainStore} from './fc3_components';
 // export {default as FindGroups} from "./find_groups";
 export {default as Settings} from "./settings";
 export {default as Home} from "./home";
