4645cba3df18906d3e41d3e9dc9a5060dca41100
Devin Ivy <devin@bigroomstudios.com>
2018-07-03 16:06:50 -0400

    Show admin messages right in page, show unread count on admin messages tab

--- src/assets/js/components/MessagesBoardConnected.vue
+++ src/assets/js/components/MessagesBoardConnected.vue
@@ -3,16 +3,29 @@
 		<div class="horizontal_tabs">
 			<ul class="tabs">
 				<li v-for="category in categories" class="tabs-title">
-					<a :href="href(category)" @click="selectTopicCategory(category)" :aria-selected="category === currentCategory">{{ category }}</a>
+					<a :href="href(category)" @click="selectTopicCategory(category)" :aria-selected="category === currentCategory">
+						{{ category }}
+						<span class="messages-unread-admin-badge" v-if="category === 'Admin Messages' && unreadAdminMessages">
+							<span v-if="unreadAdminMessages <= 9">({{ unreadAdminMessages }})</span>
+							<span v-if="unreadAdminMessages > 9">(9+)</span>
+						</span>
+					</a>
 				</li>
 			</ul>
 		</div>
 		<fc-messages-topics
+			v-if="currentCategory !== 'Admin Messages'"
 			:category="currentCategory"
 			:topics="topicsInCategory"
 			:on-click-topic="selectTopic"
 			:on-click-close="deselectTopic"
 			:topic-modal-id="modalId"
+		/>
+		<fc-messages
+			v-if="currentCategory === 'Admin Messages'"
+			show-html
+			:messages="currentMessages.reverse()"
+			:me="me"
 		/>
 		<fc-modal :custom-target="modalId" :custom-trigger="`<div style='display: none;' data-open='${modalId}'></div>`">
 			<fc-messages-board
@@ -74,26 +87,28 @@
 				'currentMessages'
 			]),
 			topicsInCategory() {
+				return this.getTopicsInCategory(this.currentCategory);
+			},
+			unreadAdminMessages() {
+				return this.getTopicsInCategory('Admin Messages').reduce((count, topic) => {
+					return count + topic.unreadCount;
+				}, 0);
+			}
+		},
+		watch: {
+			topicsInCategory([currTopic], [prevTopic]) {
 
-				const type = TOPIC_CATEGORY_MAP[this.currentCategory];
+				const isSystem = (topic) => topic && topic.topic.type === 'system';
 
-				return this.topics.filter(({ topic }) => {
+				// When transitioning to the system category, select the system topic automatically
 
-					if (topic.type !== type) {
-						return false;
-					}
+				if (!isSystem(prevTopic) && isSystem(currTopic)) {
+					this.selectTopic(currTopic);
+				}
 
-					if (type === 'post') {
-						if (this.currentCategory === 'To My Posts') {
-							return this.me.id === topic.post.user.id;
-						}
-						else {
-							return this.me.id !== topic.post.user.id;
-						}
-					}
-
-					return true;
-				});
+				if (isSystem(prevTopic) && !isSystem(currTopic)) {
+					this.deselectTopic();
+				}
 			}
 		},
 		methods: {
@@ -126,6 +141,28 @@
 				if (category in TOPIC_CATEGORY_MAP) {
 					this.currentCategory = category;
 				}
+			},
+			getTopicsInCategory(category) {
+
+				const type = TOPIC_CATEGORY_MAP[category];
+
+				return this.topics.filter(({ topic }) => {
+
+					if (topic.type !== type) {
+						return false;
+					}
+
+					if (type === 'post') {
+						if (category === 'To My Posts') {
+							return this.me.id === topic.post.user.id;
+						}
+						else {
+							return this.me.id !== topic.post.user.id;
+						}
+					}
+
+					return true;
+				});
 			}
 		}
 	}
