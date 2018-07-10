dbe58502bff28d3a7f8bc9a6aa3cf087e66d8580
Zack McCartney <zack@bigroomstudios.com>
2018-07-10 13:58:53 -0400

    Bandaids for facebook work

--- src/js/modules/home_tabs.js
+++ src/js/modules/home_tabs.js
@@ -4,12 +4,12 @@
 document.addEventListener("DOMContentLoaded", ()=> {
 	// define tabRoot to ensure strict scoping
 	const tabRoot = $('#h-example-tabs');
-	
+
 	let setActiveTab = function (elm) {
 		$(elm).attr('aria-selected', 'true');
 		$(elm).closest('li').addClass('is-active');
 	};
-	
+
 	switch (document.location.pathname) {
 		case '/home/dashboard':
 			setActiveTab('#dashboard');
@@ -23,12 +23,15 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		case '/home/my-friends':
 			setActiveTab('#my-friends');
 			break;
+		case '/home/my-friends/find': // TODO Refactor to be more flexible if more sub-pages added
+			setActiveTab('#my-friends');
+			break;
 		case '/home/my-towns':
 			setActiveTab('#my-groups');
 			break;
-			
+
 	}
-	
+
 });
 
 export default "HomeTabs";
