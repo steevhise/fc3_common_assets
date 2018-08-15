9e81ba326c5c3106e27beab66bacb07a703a4b78
Steev Hise <steev@detritus.net>
2018-08-15 14:46:49 -0700

    slight change to css re placement of post image placeholder

--- src/js/components/PostListItem.vue
+++ src/js/components/PostListItem.vue
@@ -3,7 +3,7 @@
 		<div class="post-list-item-photo">
 			<img height="128" :src="post.thumb" v-if="post.thumb">
 			<div v-else class="post-image-placeholder" >
-				<fc-icon name="chevron" :classname="`icon-chevron-${lowercase(postType)}`" style="position: absolute; width:100%; height: 100%; left: 60; top:50;" ></fc-icon>
+				<fc-icon name="chevron" :classname="`icon-chevron-${lowercase(postType)}`" style="position: absolute; width:100%; height: 100%; left: 60px; top:50px;" ></fc-icon>
 			</div>
 		</div>
 		<div class="post-list-item-content">
