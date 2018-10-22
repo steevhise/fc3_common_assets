bbaffb40be9c47a047c8a862f909ac1e519a55d0
Ryan Watts <ryandwatts@gmail.com>
2018-10-22 05:15:55 -0700

    Beta 379 Standardized FC Button using Style Guide

---
+++ src/js/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+	<button :class="`${cPallete} ${this.$attrs.class}`" v-bind="$attrs">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+	/**
+	 * This is a smart component that enables the ability to use colors from the styleguide.
+	 * Simply use default, offer, action, wanted, lend, or borrow to pull in the styling from the comp.
+	 * If you want to use a different color, you can pass an alternative class or simply use a style tag to overwrite any defaults.
+	 */
+	export default {
+		name: 'fc-button',
+		props: {
+			// comp colors include default, offer, action, wanted, lend, borrow
+			color: { type: String, default: 'default' }
+		},
+		computed: {
+			cPallete() {
+				return `btn btn-${color}`;
+			}
+		}
+	}
+</script>
