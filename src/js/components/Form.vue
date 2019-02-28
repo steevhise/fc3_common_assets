879678ff4ccf8aa8632391bed72acbcdeddfd904
Steev Hise <steev@detritus.net>
2019-02-27 21:25:15 -0700

    Changed fc-form component a bit so results coming back can be displayed right there. WIP...

--- src/js/components/Form.vue
+++ src/js/components/Form.vue
@@ -2,6 +2,8 @@
 <template>
     <form :method="method" :action="action" @change="serializeData()" @submit.prevent="handleSubmit($event)">
         <slot :formData="formData" ></slot>
+
+        <div v-if="customAlertEl" id="form-results">{{ results }}</div>
     </form>
 </template>
 
@@ -18,17 +20,37 @@
                 default: () => `/actions/`
             },
             data: {},
+            //results: {},
             customAlertEl: {
                 // takes the unique id of the element listening for form success; prevents multiple forms on same page
                 // reacting to each other's success events
-                type: Number
+                type: [Number,String]
             }
         },
+         created() {
+
+            const self = this;
+
+            if (self.customAlertEl) {
+              // handle event for legacy bus events.
+              this.$bus.$on(`formSuccess-${self.customAlertEl}`, (data) => {
+                // do stuff to a div called form-results-n if it exists.
+                console.log(data.data);
+                self.results = JSON.stringify(data.data.users);
+                console.warn(self.results);
+
+                // console.warn(`#form-results-${self.customAlertEl}`);
+                //$(`#form-results-${self.customAlertEl}`).html = ' the results:<p>' + JSON.stringify(data.data);
+              });
+            }
+
+         },
         data() {
             return {
                 formData: this.data ? JSON.parse(this.data) : {},
                 serializedData: null,
-                isSubmitted: false
+                isSubmitted: false,
+                results: null
             }
         },
         mounted() {
@@ -46,6 +68,7 @@
                 $.post(this.action, this.serializedData).done(function(data) {
 
                     if (self.customAlertEl) {
+                      console.log(`formSuccess-${self.customAlertEl}`);
                         // Allow caller to handle alert in a custom way (allows parent to take over)
                         self.$bus.$emit(`formSuccess-${self.customAlertEl}`, data);
                     } else {
@@ -62,3 +85,13 @@
         }
     }
 </script>
+
+<style scoped>
+    #form-results {
+        font-size: 10pt;
+        max-height: 10em;
+        overflow: scroll;
+    }
+
+
+</style>
