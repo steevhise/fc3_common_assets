ff9faf4ea08ec7d33a77c43ad90612f991686e0c
Charles Suggs <charles@fullsteamlabs.com>
2020-07-09 16:02:46 -0500

    Remove modtools-specific domain switching from axios.js

--- src/js/helpers/axios.js
+++ src/js/helpers/axios.js
@@ -1,20 +1,6 @@
 import axios from 'axios'
 
-export const hapiDomain = () => {
-  switch (process.env.VUE_APP_ENVIRONMENT) {
-    case 'production':
-      return 'mt.freecycle.org'
-    case 'staging':
-      return 'modstaging.freecycle.org'
-    case 'development':
-      return 'mt.freecycle.org:8000'
-    default:
-      return 'modstaging.freecycle.org'
-  }
-}
-
 export const API = axios.create({
-  baseURL: 'https://' + hapiDomain(),
   withCredentials: true
 })
 
