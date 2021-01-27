24cea5d5a52b2ea4a58ea0166d1314ffc66332af
Charles Suggs <charles@fullsteamlabs.com>
2021-01-27 12:36:47 -0600

    Adds new formatLendMessage() method to LendMessage.vue to simplify multi-layered string interpolation for translation of message in modal; also some indent cleanup #1151

--- src/js/components/LendMessage.vue
+++ src/js/components/LendMessage.vue
@@ -3,15 +3,15 @@
         <h3>{{ t("You lent your item! Want to notify your friend?") }}</h3>
         <p>{{ t("Send them a short message to keep in touch about the borrowed item") }}</p>
         <fc-form v-if="post && lendThreadId" action="/api/messaging/send"
-			:custom-alert-el="this._uid"
-			ref="lendMessageForm"
-		>
+          :custom-alert-el="this._uid"
+          ref="lendMessageForm"
+        >
             <label>{{ t("Message (1000 characters max)") }}</label>
             <input type="text"
                 maxlength="1000"
                 required
-				name="body"
-                :value="`{{ t("You're now borrowing") }} &quot;${post.subject}&quot;. {{ t("Don't forget to bring it back!") }}`"
+                name="body"
+                :value="formatLendMessage(post)"
             >
             <input type="hidden" name="threadIdentifier" :value="lendThreadId" />
             <input type="submit" class="btn-default" :value="t('Send')" />
@@ -22,28 +22,33 @@
 
 <script>
 
-	export default {
-		name: 'fc-lend-message',
-		props: {},
-		data() {
-			return {
-				post: {
-					type: Object,
-					default: () => null
-				},
-				lendThreadId: null
-			};
-		},
-		created() {
+export default {
+	name: 'fc-lend-message',
+	props: {},
+	data() {
+		return {
+			post: {
+				type: Object,
+				default: () => null
+			},
+			lendThreadId: null
+		};
+	},
+	created() {
 
-			this.$bus.$on(`formSuccess-${this._uid}`, () => {
+		this.$bus.$on(`formSuccess-${this._uid}`, () => {
 
-				this.$refs.lendMessageForm.$el.reset();
-				this.$bus.$emit('alert', { level : 'success', message: '{{ p("Message sent!") }}', timer: 20000 })
-			});
-		},
-        mounted() {
-            $('#lend-message-form').foundation();   // initialize foundation on this element, so we can be ready to open the modal
-        },
+			this.$refs.lendMessageForm.$el.reset();
+			this.$bus.$emit('alert', { level : 'success', message: '{{ t("Message sent!") }}', timer: 20000 })
+		});
+	},
+  mounted() {
+    $('#lend-message-form').foundation();   // initialize foundation on this element, so we can be ready to open the modal
+  },
+  methods: {
+	  formatLendMessage(post) {
+      `${this.t("You're now borrowing")} "${post.subject}". ${this.t("Don't forget to bring it back!")}`
+    }
 	}
+}
 </script>
