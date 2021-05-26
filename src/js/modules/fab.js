20a970c025abb6c4054f043d1ccf91a46c1a712d
Jason <jason@fullsteamlabs.com>
2021-05-26 13:23:46 -0400

    Makes fab.js trigger on both DOMContentLoaded and post-modal-loaded

--- src/js/modules/fab.js
+++ src/js/modules/fab.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+const fabHandler = () => {
 	$('.fab').on('click', function() {
 		$(this).toggleClass('open');
 		$(this).find('.text').toggleClass('open');
 		$('.option').toggleClass('open');
 		$('.close').toggleClass('open');
 	});
-});
+}
+document.addEventListener('DOMContentLoaded', fabHandler);
+document.addEventListener('post-modal-loaded', fabHandler);
 export default 'FAB';
