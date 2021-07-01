dd6324c2df723321d2d7392430ca265eafe27e55
Jason <jason@fullsteamlabs.com>
2021-07-01 15:30:08 -0400

    Resets currLimit and offset when town selection changed

--- src/js/components/Data.vue
+++ src/js/components/Data.vue
@@ -54,6 +54,11 @@
                 }, 100)
             });
 
+            this.$root.$on('townSelectionChanged', () => {
+                this.offset = 0
+                this.currLimit = this.limit
+            });
+
             this.$root.$on('loadMorePosts', () => {
                 // console.log('loadMorePosts start', self.currLimit, self.limit, self.offset, self.posts.length, self.$data.posts.length, self.count)
                 self.currLimit += Number(self.limit);
