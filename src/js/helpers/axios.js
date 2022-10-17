94fb9a99c73eef89f26d615743582211bd557cbc
Steev Hise <steev@detritus.net>
2022-10-17 15:58:21 -0700

    Changed Axios config to use our own JWT tokens.

--- src/js/helpers/axios.js
+++ src/js/helpers/axios.js
@@ -8,13 +8,14 @@ import {
     getRefreshToken
 } from 'axios-jwt';
 
-// now with automatic handling of jwt auth via Auth0! w00t!
+// now with automatic handling of jwt auth via Our own server!!!
 
-const clientCredentials = {
+const clientCredentials = {         // these are left over from the Auth0 days. except for the "a" param, which our server will want.
   client_id: 'Eoc5ukydqUDABT699fVbhKgfHmsmbXQJ',
   client_secret: 'O3GjtM5qhbnYio3GEdL8eVFLXNUjlTlT03r-F_my09tHu6NDPtW7xl8CrRVcbVfJ',
   audience: 'https://www.freecycle.org/api',
-  grant_type: 'client_credentials'
+  grant_type: 'client_credentials',
+  a: 123456             // this will not always be this...
 }
 
 export const API = axios.create({
@@ -24,26 +25,29 @@ export const API = axios.create({
 
 // Define token refresh function.
 const requestRefresh = (refresh) => {
-    console.debug('an api token refresh has been requested....', refresh);
     const token = getAccessToken(); // check that we don't already have a good token
     if (!!token) {
-        console.debug('oh we already have a token');
-        return token.access_token;
+        console.debug('token refresh: we already have a token', token);
+        return token;   // already parsed and in an "object" form in local storage.
     }
+
+    const tokenUri = window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '' ) + '/api/token';
+    console.debug(tokenUri);
+
     // Notice that this is the global axios instance, not the axiosInstance!  <-- important
-    return axios.post(`https://freecycle.eu.auth0.com/oauth/token`, clientCredentials)
-        // refresh, // not sure if we need this? a refresh token? what is that? ,
+    console.debug('getting a new jwt...');
+    return axios.post(tokenUri, clientCredentials)
+        // refresh, // not sure if we need this? a refresh token? what tf is that?
         .then((response) => {
-            console.log('we have a response from Auth0', response.data);
+            console.debug('we have a response from token endpoint', response.data);
             const tokenObject = response.data;
             // save tokens to local storage - i think we save the whole object, so that we have scope, expire times, etc
             setAuthTokens({
                 accessToken: tokenObject,
-                refreshToken: { foo: "bar"}   // we aren't using refresh tokens right now so we fake it.
+                refreshToken: { foo: "bar"}   // we aren't using refresh tokens right now, so we fake it.
             });
             setAccessToken(tokenObject);   // i think this actually loads the token for use.
-            console.log('access token: ' + tokenObject.access_token);
-            return tokenObject.access_token;   // return just the token itself
+            return tokenObject;   // return just the token itself
         });
 };
 
@@ -54,7 +58,7 @@ applyAuthTokenInterceptor(API, {
 });  // Notice that this uses the specific API instance of axios.  <-- important
 
 export const handleError = e => {
-  console.error('axios error!' + e);
+  console.error('Axios error! ' + e);
 };
 
 requestRefresh();
