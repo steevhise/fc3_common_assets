b5796163431f74d93d7ef76e927f207a1af47721
Steev Hise <steev@detritus.net>
2022-11-02 13:15:17 -0700

    still WIP. don't even try to get jwt auth to work with this commit. but i gotta switch gears..

--- src/js/helpers/axios.js
+++ src/js/helpers/axios.js
@@ -11,11 +11,10 @@ import {
 // now with automatic handling of jwt auth via Our own server!!!
 
 const clientCredentials = {         // these are left over from the Auth0 days. except for the "a" param, which our server will want.
-  client_id: 'Eoc5ukydqUDABT699fVbhKgfHmsmbXQJ',
-  client_secret: 'O3GjtM5qhbnYio3GEdL8eVFLXNUjlTlT03r-F_my09tHu6NDPtW7xl8CrRVcbVfJ',
+  developer_username: 'Eoc5ukydqUDABT699fVbhKgfHmsmbXQJ',
+  developer_password: 'O3GjtM5qhbnYio3GEdL8eVFLXNUjlTlT03r-F_my09tHu6NDPtW7xl8CrRVcbVfJ',
   audience: 'https://www.freecycle.org/api',
   grant_type: 'client_credentials',
-  code: 123456             // this will not always be this...  WIP... we never need this here, actually....
 }
 
 export const API = axios.create({
@@ -28,21 +27,30 @@ const requestRefresh = (refresh) => {
     const token = getAccessToken(); // check that we don't already have a good token
     if (!!token) {
         console.debug('token refresh: we already have a token', token);
-        return token;   // already parsed and in an "object" form in local storage.
+        if (token.exp > Date.now()) {
+            console.debug('token is good')
+            return token;
+        }
     }
 
   /*  TODO: check if token is expired  - Axios needs to do whole process:
-    1 - if token is expired, check refresh token
+    1 - access token is expired or nonexistent, check refresh token
     2 - if no refresh token or it's expired, go to auth endpoint with credentials.
     3 - if refresh token IS still good, use that at the token endpoint
 */
     // if our access token is too old, use refresh token to get a new AT.
     const refreshToken = getRefreshToken();
-    if (!!refreshToken) {
-        console.debug('refresh token we have is', refreshToken);
+    console.debug(refreshToken);
+    if (!!refreshToken && refreshToken.exp > Date.now()) {
+        console.debug('refresh token is good:', refreshToken);
+
 
     }
 
+    // we have no good refresh token either, so hit the auth endpoint and then the token endpoint.
+    const authUri = window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '' ) + '/api/auth';
+
+    // we need an auth code and a signature in the query param.
     const tokenUri = window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '' ) + '/api/token';
     console.debug(tokenUri);
 
